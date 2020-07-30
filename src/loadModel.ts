fe772cd8f314c1d9733571ce5fdc8c65b8e00001
Kevin Scott <kevin@hitheory.com>
2020-07-30 14:06:57 -0400

    Update loading of models

--- src/loadModel.ts
+++ src/loadModel.ts
@@ -1,12 +1,13 @@
 import * as tf from '@tensorflow/tfjs';
 import { IUpscalerOptions } from "./types";
+import MODELS, { DEFAULT_MODEL } from './models';
 
-const getModelPath = (model?: string) => {
-  if (model) {
-    return model;
+const getModelPath = (model: string = DEFAULT_MODEL) => {
+  if (model in MODELS) {
+    return MODELS[model].url;
   }
 
-  return 'div2k-300-2x';
+  return model;
 };
 
 const loadModel = async (opts: IUpscalerOptions) => {
--- src/models.ts
+++ src/models.ts
@@ -7,12 +7,21 @@ const buildURL = (modelFolder: string) => {
   return `${ROOT}@${version}/${MODEL_DIR}/${modelFolder}/model.json`;
 };
 
-const MODELS = {
+interface IModels {
+  [index: string]: {
+    url: string;
+  }
+}
+
+const MODELS: IModels = {
   '2x': {
     url: buildURL('div2k-006-2x'),
   },
   '3x': {
     url: buildURL('div2k-019-3x'),
+  },
+  '4x': {
+    url: buildURL('div2k-017-4x'),
   },
   'psnr_small': {
     url: buildURL('psnr-small-quant-uint8'),
@@ -20,3 +29,5 @@ const MODELS = {
 }
 
 export default MODELS;
+
+export const DEFAULT_MODEL = '2x';
