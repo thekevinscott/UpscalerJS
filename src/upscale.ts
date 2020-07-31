9397174e91727d8aa7436e0fc1127b086268dc92
Kevin Scott <kevin@hitheory.com>
2020-07-31 12:50:26 -0400

    Set images to cross origin and bump version

--- src/upscale.ts
+++ src/upscale.ts
@@ -15,6 +15,7 @@ const loadImage = (src: string): Promise<HTMLImageElement> =>
   new Promise((resolve, reject) => {
     const img = new Image();
     img.src = src;
+    img.crossOrigin = 'anonymous';
     img.onload = () => resolve(img);
     img.onerror = reject;
   });
