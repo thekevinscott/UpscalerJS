6db4c1c462335794dbace10daea3c94aec3c7f07
Kevin Scott <kevin@hitheory.com>
2020-07-31 15:20:34 -0400

    Make edits to examples

--- examples/tensor/index.js
+++ examples/tensor/index.js
@@ -1,20 +1,26 @@
-import * as tf from '@tensorflow/tfjs';
-import Upscaler from 'upscaler';
-const button = document.getElementById('button');
-const info = document.getElementById('info');
+import * as tf from "@tensorflow/tfjs";
+import flower from "./flower.png";
+import Upscaler from "upscaler";
+const button = document.getElementById("button");
+const info = document.getElementById("info");
 
 const upscaler = new Upscaler({
-  model: '2x',
+  model: "2x"
 });
 button.onclick = () => {
-  info.innerText = 'Upscaling...';
+  info.innerText = "Upscaling...";
   const img = new Image();
-  img.src = flower.src;
-  const pixels = tf.browser.fromPixels(img);
-  upscaler.upscale(pixels, {
-    output: 'tensor',
-  }).then((upscaledImg) => {
-    upscaledImg.print();
-    info.innerText = 'Open your console to see the tensor.';
-  });
+  img.src = flower;
+  img.crossOrigin = "anonymous";
+  img.onload = () => {
+    const pixels = tf.browser.fromPixels(img);
+    upscaler
+      .upscale(pixels, {
+        output: "tensor"
+      })
+      .then(upscaledImg => {
+        upscaledImg.print();
+        info.innerText = "Open your console to see the tensor.";
+      });
+  };
 };
--- examples/upload/index.js
+++ examples/upload/index.js
@@ -1,36 +1,37 @@
-import Upscaler from 'upscaler';
-const table = document.getElementById('table');
-const original = document.getElementById('original');
-const target = document.getElementById('target');
-const file = document.getElementById('file');
-const info = document.getElementById('info');
+import * as tf from "@tensorflow/tfjs";
+import Upscaler from "upscaler";
+const table = document.getElementById("table");
+const original = document.getElementById("original");
+const target = document.getElementById("target");
+const file = document.getElementById("file");
+const info = document.getElementById("info");
 
 const createImage = (targetDiv, src) => {
-  const img = document.createElement('img');
+  const img = document.createElement("img");
   img.src = src;
-  targetDiv.innerHTML = '';
+  targetDiv.innerHTML = "";
   targetDiv.appendChild(img);
   return img;
-}
+};
 
 const upscaler = new Upscaler({
-  model: '2x',
+  model: "2x"
 });
-file.addEventListener("change", handleFiles, false);
-function handleFiles() {
-  info.innerText = 'Upscaling...';
-  table.style = '';
-
+async function handleFiles() {
+  info.innerText = "Upscaling...";
+  table.style = "";
+  await tf.nextFrame();
   const file = this.files[0];
   const fr = new FileReader();
-  fr.onload = () => {
+  fr.onload = async () => {
     const img = createImage(original, fr.result);
     const start = new Date().getTime();
-    upscaler.upscale(img).then((upscaledImgSrc) => {
-      createImage(target, upscaledImgSrc);
-      const ms = new Date().getTime() - start;
-      info.innerText = `Upscaled in ${ms} ms`;
-    });
+    const upscaledImgSrc = await upscaler.upscale(img);
+    createImage(target, upscaledImgSrc);
+    const ms = new Date().getTime() - start;
+    info.innerText = `Upscaled in ${ms} ms`;
   };
   fr.readAsDataURL(file);
-};
+}
+
+file.addEventListener("change", handleFiles, false);
