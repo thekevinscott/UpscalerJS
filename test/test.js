274a9f9389f5e55ef1759c8b614f1c91dfab3fbb
Kevin Scott <kevin@hitheory.com>
2021-02-27 12:17:48 -0600

    Add a fake title

--- test/test.js
+++ test/test.js
@@ -34,30 +34,33 @@ const getDriverForCapabilities = (capabilities) => {
     .build();
 };
 
-describe("integration tests", () => {
-  let driver;
-
-  beforeAll(async () => {
-    // driver = getDriverForCapabilities(CAPABILITIES[0]);
-
-// Input capabilities
-var capabilities = {
-  'os': 'windows',
-  'os_version': '10',
-  'browserName': 'chrome',
-  'browser_version' : 'latest',
+const DEFAULT_CAPABILITIES = {
+  'browser_version': 'latest',
   'browserstack.local': 'true',
   'build': process.env.BROWSERSTACK_BUILD_NAME,
   'project': process.env.BROWSERSTACK_PROJECT_NAME,
   'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
-}
+};
 
-driver = new webdriver.Builder()
-  .usingServer('http://hub-cloud.browserstack.com/wd/hub')
-  .withCapabilities(capabilities)
-  .build();
+describe("integration tests", () => {
+  let driver;
+
+  beforeAll(async () => {
+    // driver = getDriverForCapabilities(CAPABILITIES[0]);
+
+    const capabilities = {
+      'os': 'windows',
+      'os_version': '10',
+      'browserName': 'chrome',
+      ...DEFAULT_CAPABILITIES,
+    };
+
+    driver = new webdriver.Builder()
+      .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+      .withCapabilities(capabilities)
+      .build();
   });
 
   afterAll(async () => {
