1568393749cb4b594cb1fda7347dd65e24a180fd
Kevin Scott <kevin@hitheory.com>
2021-02-27 12:14:30 -0600

    Try manually setting driver in beforeAll

--- test/test.js
+++ test/test.js
@@ -38,7 +38,26 @@ describe("integration tests", () => {
   let driver;
 
   beforeAll(async () => {
-    driver = getDriverForCapabilities(CAPABILITIES[0]);
+    // driver = getDriverForCapabilities(CAPABILITIES[0]);
+
+// Input capabilities
+var capabilities = {
+  'os': 'windows',
+  'os_version': '10',
+  'browserName': 'chrome',
+  'browser_version' : 'latest',
+  'browserstack.local': 'true',
+  'build': process.env.BROWSERSTACK_BUILD_NAME,
+  'project': process.env.BROWSERSTACK_PROJECT_NAME,
+  'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
+  'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
+}
+
+var driver = new webdriver.Builder()
+  .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+  .withCapabilities(capabilities)
+  .build();
   });
 
   afterAll(async () => {
@@ -46,8 +65,13 @@ describe("integration tests", () => {
   });
 
   it("test", async () => {
+    console.log('test 1')
+    console.log(driver);
     await driver.get('http://localhost:8099');
+    console.log('test 2')
     const title = await driver.getTitle();
+    console.log('test 3')
     expect(title).to.equal('Some title');
+    console.log('test 4')
   });
 });
