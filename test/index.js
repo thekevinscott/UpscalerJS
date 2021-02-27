a5d1778aae7c1e4fbb26526e15d6c927eeb77c97
Kevin Scott <kevin@hitheory.com>
2021-02-27 11:40:54 -0600

    Move multiple browser configuration into test script

--- test/index.js
+++ test/index.js
@@ -1,27 +1,28 @@
 const webdriver = require('selenium-webdriver');
 
-// Input capabilities
-const capabilities = {
+const CAPABILITIES = [
+{
   'os': 'windows',
   'os_version': '10',
   'browserName': 'chrome',
   'browser_version' : 'latest',
   'browserstack.local': 'true',
+}
+]
+
+CAPABILITIES.map(capabilities => ({
+  ...capabilities,
   'build': process.env.BROWSERSTACK_BUILD_NAME,
   'project': process.env.BROWSERSTACK_PROJECT_NAME,
   'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
-}
+})).forEach(async capabilities => {
+  const driver = new webdriver.Builder()
+    .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+    .withCapabilities(capabilities)
+    .build()
+    .get('http://localhost:8099');
 
-const driver = new webdriver.Builder()
-  .usingServer('http://hub-cloud.browserstack.com/wd/hub')
-  .withCapabilities(capabilities)
-  .build();
-
-driver.get('http://localhost:8099').then(function () {
-  driver.getTitle().then(function (title) {
-    console.log(title);
-    driver.quit();
-  });
+  driver.quit();
 });
