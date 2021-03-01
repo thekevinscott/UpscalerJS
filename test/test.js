591c1f42c515c519d2bb9d2745ab6dbd8eb9af8c
Kevin Scott <kevin@hitheory.com>
2021-02-28 19:58:43 -0600

    wip

--- test/test.js
+++ test/test.js
@@ -8,40 +8,6 @@ const handler = require('serve-handler');
 const http = require('http');
 jest.setTimeout(30000);
 
-const CAPABILITIES = [
-{
-  'os': 'windows',
-  'os_version': '10',
-  'browserName': 'chrome',
-},
-{
-  'os': 'windows',
-  'os_version': '10',
-  'browserName': 'firefox',
-}, 
-{
-  'os': 'windows',
-  'os_version': '8.1',
-  'browserName': 'chrome',
-},
-];
-
-const getDriverForCapabilities = (capabilities) => {
-  return new webdriver.Builder()
-    .usingServer('http://hub-cloud.browserstack.com/wd/hub')
-    .withCapabilities({
-      'browser_version': 'latest',
-      'browserstack.local': 'true',
-      'build': process.env.BROWSERSTACK_BUILD_NAME,
-      'project': process.env.BROWSERSTACK_PROJECT_NAME,
-      'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
-      'browserstack.user': process.env.BROWSERSTACK_USERNAME,
-      'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
-      ...capabilities,
-    })
-    .build();
-};
-
 const DEFAULT_CAPABILITIES = {
   'browser_version': 'latest',
   'browserstack.local': 'true',
@@ -68,11 +34,12 @@ describe.each([
   let bsLocal;
   let server;
 
-  beforeAll(async () => {
-    const server = http.createServer((request, response) => handler(request, response, {
+  const PORT = 8099;
+
+  beforeAll(async (done) => {
+    server = http.createServer((request, response) => handler(request, response, {
       public: 'test/server',
     }));
-    server.listen(8099);
 
     if (argv.ci !== true) {
       bsLocal = new browserstack.Local();
@@ -89,19 +56,24 @@ describe.each([
         ...DEFAULT_CAPABILITIES,
       })
       .build();
+
+    server.listen(PORT, done);
   });
 
-  afterAll(async () => {
+  afterAll(async (done) => {
     await driver.quit();
     if (argv.ci !== true && bsLocal && bsLocal.isRunning()) {
       bsLocal.stop(() => {});
     }
-    server.stop();
+    server.close(done);
   });
 
   it("test", async () => {
-    await driver.get('http://localhost:8099');
-    const title = await driver.getTitle();
-    expect(title).toEqual('Some title');
+    await driver.get(`http://localhost:${PORT}`);
+    const img = await driver.findElement(webdriver.By.css('img'))
+
+    const return_value = await driver.executeScript('return Upscaler');
+    expect(return_value).toEqual(2);
+    console.log('done!')
   });
 });
