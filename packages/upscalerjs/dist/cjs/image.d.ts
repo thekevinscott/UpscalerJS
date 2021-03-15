518e461eb740afddb843b872654313f21f43385f
Kevin Scott <kevin@hitheory.com>
2021-03-15 08:16:15 -0500

    Add sample github workflows for sauce

--- packages/upscalerjs/dist/cjs/image.d.ts
+++
@@ -1,6 +0,0 @@
-import * as tf from '@tensorflow/tfjs';
-export declare const loadImage: (src: string) => Promise<HTMLImageElement>;
-export declare const getImageAsPixels: (pixels: string | HTMLImageElement | tf.Tensor) => Promise<{
-    tensor: tf.Tensor4D;
-    type: 'string' | 'HTMLImageElement' | 'tensor';
-}>;
--- packages/upscalerjs/dist/cjs/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var upscaler_1 = require("./upscaler");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return upscaler_1.default; } });
