d103902eac19e8cf1f85df90c359fa59d21de8fd
Kevin Scott <kevin@hitheory.com>
2021-03-15 19:30:36 -0500

    Try cloud on github

--- packages/upscalerjs/tests/example.test.js
+++ packages/upscalerjs/tests/example.test.js
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+console.log(process.env)
+console.log(process.env.platform)
+const PLATFORM = process.env.platform;
 const { it, expect } = require('@playwright/test');
 
 it('is a basic test with the page', async ({ page }) => {
@@ -22,7 +26,7 @@ it('is a basic test with the page', async ({ page }) => {
 
 it('tests the local server', async ({ browser }) => {
   const page = await browser.newPage();
-  await page.goto('http://host.docker.internal:8000');
+  await page.goto(PLATFORM === 'cloud' ? 'http://localhost:8000' : 'http://host.docker.internal:8000');
   const title = await page.title();
   expect(title).toBe('UpscalerJS Integration Test Webpack Bundler Server')
 });
