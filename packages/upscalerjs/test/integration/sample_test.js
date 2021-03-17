8d5e41a8b5d89315baa1e3ca1ac2bddd723cda4c
Kevin Scott <kevin@hitheory.com>
2021-03-17 18:41:59 -0500

    Fix sample test

--- packages/upscalerjs/test/integration/sample_test.js
+++ packages/upscalerjs/test/integration/sample_test.js
@@ -9,30 +9,31 @@ const capabilities = {
  'name': 'BStack-[NodeJS] Sample Test', // test name
  'build': 'BStack Build Number 1' // CI/CD job or build name
 }
-async function runTestWithCaps () {
-  let driver = new webdriver.Builder()
-    .usingServer('http://kevinscott3:VqZ44zEoYmzqzdhFDrRM@hub-cloud.browserstack.com/wd/hub')
-    .withCapabilities(capabilities)
-    .build();
-  await driver.get("http://www.google.com");
-  const inputField = await driver.findElement(webdriver.By.name("q"));
-  await inputField.sendKeys("BrowserStack", webdriver.Key.ENTER); // this submits on desktop browsers
-  try {
-    await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
-  } catch (e) {
-    await inputField.submit(); // this helps in mobile browsers
-  }
-  try {
-    await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
-    console.log(await driver.getTitle());
-    await driver.executeScript(
-      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title contains BrowserStack!"}}'
-    );
-  } catch (e) {
-    await driver.executeScript(
-      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Page could not load in time"}}'
-    );
-  }
-  await driver.quit();
-}
-runTestWithCaps(); 
+describe('sample', () => {
+  it('tests a sample', async () => {
+    let driver = new webdriver.Builder()
+      .usingServer('http://kevinscott3:VqZ44zEoYmzqzdhFDrRM@hub-cloud.browserstack.com/wd/hub')
+      .withCapabilities(capabilities)
+      .build();
+    await driver.get("http://www.google.com");
+    const inputField = await driver.findElement(webdriver.By.name("q"));
+    await inputField.sendKeys("BrowserStack", webdriver.Key.ENTER); // this submits on desktop browsers
+    try {
+      await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
+    } catch (e) {
+      await inputField.submit(); // this helps in mobile browsers
+    }
+    try {
+      await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
+      console.log(await driver.getTitle());
+      await driver.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title contains BrowserStack!"}}'
+      );
+    } catch (e) {
+      await driver.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Page could not load in time"}}'
+      );
+    }
+    await driver.quit();
+  });
+});
