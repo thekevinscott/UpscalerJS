4a3bd28d895204f3d202d7da005eeb3d62d9bd0a
Kevin Scott <kevin@hitheory.com>
2021-03-18 08:49:55 -0500

    Add additional console logs

--- packages/test-scaffolding/server.js
+++ packages/test-scaffolding/server.js
@@ -10,9 +10,12 @@ const DIST = path.join(ROOT, '/dist');
 
 let compiler = undefined;
 const bundle = () => new Promise((resolve, reject) => {
+  console.log('bundle 1')
   rimraf.sync(DIST);
 
+  console.log('bundle 2')
   if (compiler === undefined) {
+    console.log('bundle 3')
     const entryFiles = path.join(ROOT, 'index.js');
 
     compiler = webpack({
@@ -37,7 +40,9 @@ const bundle = () => new Promise((resolve, reject) => {
     });
   }
 
+  console.log('bundle 4')
   compiler.run((err, stats) => {
+    console.log('bundle 5')
     if (err || stats.hasErrors()) {
       reject(err || stats.toJson('errors-only').errors.map(e => e.message));
     } else {
@@ -48,15 +53,21 @@ const bundle = () => new Promise((resolve, reject) => {
 
 module.exports.startServer = (PORT, callback) => new Promise(async resolve => {
   console.log('starting server 1')
-  await bundle();
-  console.log('starting server 2')
-  const server = http.createServer((request, response) => handler(request, response, {
-    public: DIST,
-  }));
-  console.log('starting server 3')
-  server.listen(PORT, () => {
-    console.log('starting server 4')
-    if (callback) { callback(); }
-    resolve(server);
-  });
-})
+  console.log(bundle);
+  try {
+    await bundle();
+    console.log('starting server 2')
+    const server = http.createServer((request, response) => handler(request, response, {
+      public: DIST,
+    }));
+    console.log('starting server 3')
+    server.listen(PORT, () => {
+      console.log('starting server 4')
+      if (callback) { callback(); }
+      resolve(server);
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+});
