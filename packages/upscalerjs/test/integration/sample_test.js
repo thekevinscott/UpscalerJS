038de764971b5709ad2a06bb6b38860b1f4b6982
Kevin Scott <kevin@hitheory.com>
2021-03-17 19:25:19 -0500

    Fix test

--- packages/upscalerjs/test/integration/sample_test.js
+++ packages/upscalerjs/test/integration/sample_test.js
@@ -9,24 +9,35 @@ const capabilities = {
  'name': 'BStack-[NodeJS] Sample Test', // test name
  'build': 'BStack Build Number 1' // CI/CD job or build name
 }
+
+const username = process.env.BROWSERSTACK_USERNAME;
+const accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
+console.log(username, accessKey);
 describe('sample', () => {
   let driver;
 
   beforeAll(async () => {
     driver = new webdriver.Builder()
-      .usingServer('http://kevinscott3:VqZ44zEoYmzqzdhFDrRM@hub-cloud.browserstack.com/wd/hub')
+      .usingServer(`http://${username}:${accessKey}@hub-cloud.browserstack.com/wd/hub`)
       .withCapabilities(capabilities)
       .build();
- 
-  }, 60000);
- 
+  }, 60000)
+
   afterAll(async () => {
     await driver.quit();
   }, 60000);
 
   it('tests a sample', async () => {
-    await driver.get('https://www.selenium.dev');
-    const title = await driver.findElement(by.tagName('h1')).getText()
-    expect(title).toContain('SeleniumHQ Browser Automation')
-  }, 60000);
+    await driver.get("http://www.google.com");
+    const inputField = await driver.findElement(webdriver.By.name("q"));
+    await inputField.sendKeys("BrowserStack", webdriver.Key.ENTER); // this submits on desktop browsers
+    try {
+      await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
+    } catch (e) {
+      await inputField.submit(); // this helps in mobile browsers
+    }
+    await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
+    const title = await driver.getTitle();
+    expect(title).toEqual('BrowserStack - Google Search');
+  }, 120000);
 });
