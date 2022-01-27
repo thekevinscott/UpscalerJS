55ea589eff0bfed71f5ad7af49869f7ef275ccd3
Kevin Scott <kevin@hitheory.com>
2022-01-27 07:36:47 -0500

    Fix node test

--- test/lib/node/prepare.ts
+++ test/lib/node/prepare.ts
@@ -1,17 +1,17 @@
 import callExec from "../utils/callExec";
 import * as path from 'path';
 import * as rimraf from 'rimraf';
-import * as fs from 'fs';
-import { getTFJSVersion } from "../utils/getTFJSVersion";
-// import { copyFixtures } from "../utils/copyFixtures";
 
 const ROOT = path.join(__dirname);
-// export const DIST = path.join(ROOT, '/dist');
 const SRC = path.join(ROOT, '/src');
 const NODE_MODULES = path.join(ROOT, '/node_modules');
 const UPSCALER_PATH = path.join(ROOT, '../../../packages/upscalerjs')
 
 export const prepareScriptBundleForCJS = async () => {
+  await callExec('yarn', {
+    cwd: ROOT,
+  });
+
   rimraf.sync(`${NODE_MODULES}/upscaler`);
 
   await callExec(`cp -r ${UPSCALER_PATH} ${NODE_MODULES}/upscaler`, {
@@ -20,11 +20,8 @@ export const prepareScriptBundleForCJS = async () => {
 };
 
 export const executeNodeScript = async (scriptPath: string, args: string = '') => {
-  await callExec('yarn', {
-    cwd: ROOT,
-  });
   let data = '';
-  await callExec(`node "${scriptPath}" ${args}`, {
+  await callExec(`node "./src/upscale_image.js" ${args}`, {
     cwd: ROOT
   }, chunk => {
     data += chunk;
