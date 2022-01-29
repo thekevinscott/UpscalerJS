2b5dad538b27af558cecdcd0ec789281e390f8e5
Kevin Scott <kevin@hitheory.com>
2022-01-29 10:11:41 -0500

    Clean up script

--- scripts/watch-filter.js
+++ scripts/watch-filter.js
@@ -1,29 +1,25 @@
 const path = require('path');
 const SRC = path.resolve(__dirname, '../packages/upscalerjs/src');
-const EXCLUDES = [
-  'tfjs.generated.ts',
+const EXCLUDED_SUFFIXES = [
+  'generated.ts',
   'test.ts',
 ];
-const matches = (rootDir, fileOrDir) => {
+
+const match = (rootDir, fileOrDir) => {
   const min = Math.min(rootDir.length, fileOrDir.length);
   const isRootMatch = rootDir.substring(0, min) === fileOrDir.substring(0, min);
   if (!isRootMatch) {
     return false;
   }
 
-  for (let i = 0; i < EXCLUDES.length; i++) {
-    const excludes = EXCLUDES[i];
+  for (let i = 0; i < EXCLUDED_SUFFIXES.length; i++) {
+    const excludes = EXCLUDED_SUFFIXES[i];
 
     if (fileOrDir.endsWith(excludes)) {
       return false;
     }
   }
   return true;
-}
-module.exports = (file) => {
-  const isMatch = matches(SRC, file);
-  if (isMatch) {
-    console.log('check?', file)
-  }
-  return isMatch;
-}
+};
+
+module.exports = (file) => match(SRC, file);
