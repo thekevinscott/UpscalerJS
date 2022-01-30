efde95ed18f54e10b4914d8acd0dcfc557684ea1
Kevin Scott <kevin@hitheory.com>
2022-01-30 09:00:54 -0500

    Adding model tests in node

--- test/integration/browser/model.ts
+++ test/integration/browser/model.ts
@@ -46,71 +46,86 @@ describe('Model Loading Integration Tests', () => {
 
   const PORT = 8099;
 
-  // beforeAll(async function beforeAll(done) {
-  //   const start = new Date().getTime();
-  //   const startBrowserStack = async () => {
-  //     bsLocal = new browserstack.Local();
-  //     await startBsLocal(bsLocal);
-  //   };
+  beforeAll(async function beforeAll(done) {
+    const start = new Date().getTime();
+    const startBrowserStack = async () => {
+      bsLocal = new browserstack.Local();
+      await startBsLocal(bsLocal);
+    };
 
-  //   const startServerWrapper = async () => {
-  //     await bundle();
-  //     server = await startServer(PORT, DIST);
-  //   };
+    const startServerWrapper = async () => {
+      await bundle();
+      server = await startServer(PORT, DIST);
+    };
 
-  //   await Promise.all([
-  //     startBrowserStack(),
-  //     startServerWrapper(),
-  //   ]);
+    await Promise.all([
+      startBrowserStack(),
+      startServerWrapper(),
+    ]);
 
-  //     driver = new webdriver.Builder()
-  //       .usingServer(serverURL)
-  //       .withCapabilities(DEFAULT_CAPABILITIES)
-  //       .build();
+      driver = new webdriver.Builder()
+        .usingServer(serverURL)
+        .withCapabilities(DEFAULT_CAPABILITIES)
+        .build();
 
-  //   const end = new Date().getTime();
-  //   if (TRACK_TIME) {
-  //     console.log(`Completed pre-test scaffolding in ${Math.round((end - start) / 1000)} seconds`);
-  //   }
-  //   done();
-  // });
+    const end = new Date().getTime();
+    if (TRACK_TIME) {
+      console.log(`Completed pre-test scaffolding in ${Math.round((end - start) / 1000)} seconds`);
+    }
+    done();
+  });
 
-  // afterAll(async function afterAll(done) {
-  //   const start = new Date().getTime();
-  //   const stopBrowserstack = () => new Promise(resolve => {
-  //     if (bsLocal && bsLocal.isRunning()) {
-  //       bsLocal.stop(resolve);
-  //     }
-  //   });
+  afterAll(async function afterAll(done) {
+    const start = new Date().getTime();
+    const stopBrowserstack = () => new Promise(resolve => {
+      if (bsLocal && bsLocal.isRunning()) {
+        bsLocal.stop(resolve);
+      }
+    });
 
-  //   const stopServer = () => new Promise((resolve) => {
-  //     if (server) {
-  //       server.close(resolve);
-  //     } else {
-  //       console.warn('No server found')
-  //       resolve();
-  //     }
-  //   });
-  //   await Promise.all([
-  //     stopBrowserstack(),
-  //     stopServer(),
-  //     driver.quit(),
-  //   ]);
-  //   const end = new Date().getTime();
-  //   if (TRACK_TIME) {
-  //     console.log(`Completed post-test clean up in ${Math.round((end - start) / 1000)} seconds`);
-  //   }
-  //   done();
-  // });
+    const stopServer = () => new Promise((resolve) => {
+      if (server) {
+        server.close(resolve);
+      } else {
+        console.warn('No server found')
+        resolve();
+      }
+    });
+    await Promise.all([
+      stopBrowserstack(),
+      stopServer(),
+      driver.quit(),
+    ]);
+    const end = new Date().getTime();
+    if (TRACK_TIME) {
+      console.log(`Completed post-test clean up in ${Math.round((end - start) / 1000)} seconds`);
+    }
+    done();
+  });
 
-  // beforeEach(async function beforeEach() {
-  //   await driver.get(`http://localhost:${PORT}`);
-  // });
+  beforeEach(async function beforeEach() {
+    await driver.get(`http://localhost:${PORT}`);
+  });
 
-  // it("loads a local model", async () => {
-  //   const result = await driver.executeScript(() => {
-  //     return window['upscaler'].upscale(window['flower']);
-  //   });
-  //   checkImage(result, "upscaled-4x-pixelator.png", 'diff.png');
-  // });
+  it("loads a locally exposed model via implied HTTP", async () => {
+    const result = await driver.executeScript(() => {
+      const upscaler = new window['Upscaler']({
+        model: '/pixelator/pixelator.json',
+        scale: 4,
+      });
+      return upscaler.upscale(window['flower']);
+    });
+    checkImage(result, "upscaled-4x-pixelator.png", 'diff.png');
+  });
+
+  it("loads a locally exposed model via absolute HTTP", async () => {
+    const result = await driver.executeScript(() => {
+      const upscaler = new window['Upscaler']({
+        model: `${window.location.origin}/pixelator/pixelator.json`,
+        scale: 4,
+      });
+      return upscaler.upscale(window['flower']);
+    });
+    checkImage(result, "upscaled-4x-pixelator.png", 'diff.png');
+  });
 });
