ff9eec56bcd154ba06aef64a03e3ff43e255b2b7
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-03-03 10:06:54 -0500

    Update version script (#239)
    * Update version script to also update dependencies

--- scripts/package-scripts/update-version.ts
+++ scripts/package-scripts/update-version.ts
@@ -5,8 +5,10 @@ import findAllPackages from './find-all-packages';
 import isValidVersion from './utils/isValidVersion';
 import execute from './utils/execute';
 
+type PackageJson = Record<string, any>;
 type Package = 'UpscalerJS' | 'Models' | 'Examples' | 'Root';
-type Answers = { packages: Array<Package>, version: string, commit: boolean }
+type Answers = { packages: Array<Package>, version: string, commit: boolean, updateDependencies?: boolean, }
+type GetMessage = (file: string) => string;
 
 const ROOT_DIR = path.resolve(__dirname, '../..');
 const PACKAGES_DIR = path.resolve(ROOT_DIR, 'packages');
@@ -18,22 +20,34 @@ const getFormattedName = (file: string) => {
   return file.split(`${ROOT_DIR}/`).pop();
 };
 
-const updateMultiplePackages = async (dir: string, version: string, commit: boolean) => {
+type TransformFn = (packageJSON: PackageJson, version: string) => PackageJson;
+const updateMultiplePackages = async (dir: string, version: string, commit: boolean, transform?: TransformFn, getMessage: GetMessage = defaultGetMessage) => {
   const packages = findAllPackages(dir);
   for (let i = 0; i < packages.length; i++) {
     const pkg = packages[i];
-    await updateSinglePackage(pkg, version, commit);
+    await updateSinglePackage(pkg, version, commit, transform, getMessage);
   }
 };
 
-const updateSinglePackage = async (dir: string, version: string, commit: boolean) => {
-  const packageJSON = getPackageJSON(dir);
+const defaultTransform: TransformFn = (packageJSON, version) => {
   packageJSON.version = version;
-  writePackageJSON(dir, packageJSON);
+  return packageJSON;
+}
+
+const defaultGetMessage: GetMessage = (file: string) => {
+  if (file.endsWith('package.json')) {
+    return `- Updated ${getFormattedName(file)}`;
+  }
+  return `- Updated ${getFormattedName(`${file}/package.json`)}`;
+}
+
+const updateSinglePackage = async (dir: string, version: string, commit: boolean, transform: TransformFn = defaultTransform, getMessage: GetMessage = defaultGetMessage) => {
+  const packageJSON = getPackageJSON(dir);
+  writePackageJSON(dir, transform(packageJSON, version));
   if (commit) {
     await commitPackageJSON(dir);
   }
-  console.log(`- Updated ${getFormattedName(dir)}`);
+  console.log(getMessage(dir));
 };
 
 const getPackageJSONPath = (file: string) => {
@@ -71,6 +85,11 @@ const getCurrentVersions = () => {
   ].join(' | ');
 };
 
+const UPSCALER_JS = 'UpscalerJS';
+const ROOT = 'Root';
+const EXAMPLES = 'Examples';
+const MODELS = 'Models';
+const AVAILABLE_PACKAGES = [ UPSCALER_JS, MODELS, EXAMPLES, ROOT];
 
 const updateVersion = () => new Promise(resolve => {
   inquirer.prompt<Answers>([
@@ -83,9 +102,14 @@ const updateVersion = () => new Promise(resolve => {
       type: 'checkbox',
       name: 'packages',
       message: 'Which packages do you want to update?',
-      choices: [
-        'UpscalerJS', 'Models', 'Examples', 'Root',
-      ],
+      choices: AVAILABLE_PACKAGES,
+    },
+    {
+      name: 'updateDependencies',
+      message: `Since UpscalerJS's version will be updated, do you also want to update packages (like examples) that reference it?`,
+      type: 'confirm',
+      default: true,
+      when: ({ packages }: Omit<Answers, 'updateDependencies'>) => packages.includes('UpscalerJS'),
     },
     {
       name: 'commit',
@@ -93,7 +117,7 @@ const updateVersion = () => new Promise(resolve => {
       type: 'confirm',
       default: true,
     },
-  ]).then(async ({ version, packages, commit }) => {
+  ]).then(async ({ version, packages, commit, updateDependencies }) => {
     if (!isValidVersion(version)) {
       throw new Error(`Version is not in the format x.x.x. You specified: ${version}`);
     }
@@ -103,16 +127,25 @@ const updateVersion = () => new Promise(resolve => {
     }
 
     await Promise.all(packages.map(packageKey => {
-      if (packageKey === 'Examples') {
+      if (packageKey === EXAMPLES) {
         return updateMultiplePackages(EXAMPLES_DIR, version, commit);
-      } else if (packageKey === 'Models') {
+      } else if (packageKey === MODELS) {
         return updateSinglePackage(MODELS_DIR, version, commit);
-      } else if (packageKey === 'UpscalerJS') {
+      } else if (packageKey === UPSCALER_JS) {
         return updateSinglePackage(UPSCALERJS_DIR, version, commit);
-      } else if (packageKey === 'Root') {
+      } else if (packageKey === ROOT) {
         return updateSinglePackage(ROOT_DIR, version, commit);
       }
     }));
+    if (updateDependencies) {
+      return updateMultiplePackages(EXAMPLES_DIR, version, commit, packageJSON => {
+        const deps = packageJSON.dependencies;
+        deps['upscaler'] = version;
+        return packageJSON;
+      }, dir => {
+        return `- Updated "upscaler" dependency in ${getFormattedName(dir)}`;
+      });
+    }
     if (commit) {
       const cmd = `git commit -m "Updated version to ${version} for ${formatArray(packages)}"`;
       await execute(cmd);
