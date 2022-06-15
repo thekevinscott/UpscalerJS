c80984fa812303a27b0624876a25bf0188a557f1
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-06-15 13:23:29 -0400

    Remove node src folder

--- test/lib/node/src/modelDefinitions.js
+++
@@ -1,7 +0,0 @@
-const Upscaler = require('upscaler-for-node/node');
-
-(async () => {
-  const upscaler = new Upscaler();
-  const data = await upscaler.getModelDefinitions();
-  console.log(`OUTPUT: ${JSON.stringify(data)}`);
-})();
