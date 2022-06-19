c42eae67667dfc2e204a71695164d37ad7a4f77b
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-06-19 08:02:26 -0400

    Restore default model

--- packages/upscalerjs/src/upscaler.ts
+++ packages/upscalerjs/src/upscaler.ts
@@ -11,9 +11,9 @@ import { loadModel, } from './loadModel.generated';
 import warmup from './warmup';
 import { cancellableUpscale, } from './upscale';
 import type { GetImageAsTensorInput, } from './image.generated';
-// import ESRGANSlim from '@upscalerjs/esrgan-legacy';
+import ESRGANSlim from '@upscalerjs/esrgan-slim';
 
-// const DEFAULT_MODEL = ESRGANSlim;
+const DEFAULT_MODEL = ESRGANSlim;
 
 export class Upscaler {
   _opts: UpscalerOptions;
@@ -27,8 +27,7 @@ export class Upscaler {
     this._opts = {
       ...opts,
     };
-    // this._model = loadModel(this._opts.model || DEFAULT_MODEL);
-    this._model = loadModel(this._opts.model as ModelDefinition);
+    this._model = loadModel(this._opts.model || DEFAULT_MODEL);
     void warmup(this._model, this._opts.warmupSizes || []);
   }
 
