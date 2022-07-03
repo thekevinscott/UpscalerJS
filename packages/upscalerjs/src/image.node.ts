67108bba7c607c1f4cb07e91504ca24c83650151
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-03 10:40:16 -0400

    Avoid wildcard import

--- packages/upscalerjs/src/image.node.ts
+++ packages/upscalerjs/src/image.node.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import fs from 'fs';
 import { tf, } from './dependencies.generated';
 import { isFourDimensionalTensor, isThreeDimensionalTensor, isTensor, isString, } from './utils';
 
