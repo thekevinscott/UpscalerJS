f8c7dd80d5f3e432870394f7f44fdf02f2e753bd
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-03 09:38:17 -0400

    Fix logger

--- scripts/package-scripts/update-tfjs.ts
+++ scripts/package-scripts/update-tfjs.ts
@@ -50,7 +50,7 @@ const makeSetVersionForPackageJSON = (version: string): TransformPackageJsonFn =
         const [key] = value;
         deps[key] = version;
         value = gen.next().value;
-        updates.push(`  - ${depKey}: ${key}`);
+        updates = updates.concat([`  - ${depKey}: ${key}`]);
       }
       packageJSON[depKey] = deps;
     }
