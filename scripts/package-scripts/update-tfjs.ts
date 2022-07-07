56494c5b7e88e1a82d57810edecd72747f37066f
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-07 15:01:57 -0400

    wip

--- scripts/package-scripts/update-tfjs.ts
+++ scripts/package-scripts/update-tfjs.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import inquirer from 'inquirer';
 import isValidVersion from './utils/isValidVersion';
-import { AVAILABLE_PACKAGES, DIRECTORIES, getPackageJSON, getPackageJSONPath, getPackageJSONValue, getPreparedFolderName, Package, PackageUpdaterLogger, ROOT, TransformPackageJsonFn, updateMultiplePackages, updateSinglePackage, UPSCALER_JS } from './utils/packages';
+import { AVAILABLE_PACKAGES, DIRECTORIES, getPackageJSON, getPackageJSONPath, getPackageJSONValue, getPreparedFolderName, Package, PackageUpdaterLogger, ROOT, TransformPackageJsonFn, updateMultiplePackages, updatePackageJSONForKey, updateSinglePackage, UPSCALER_JS } from './utils/packages';
 
 type Answers = { packages: Array<Package>, version: string}
 
@@ -23,7 +23,7 @@ const makeSetVersionForPackageJSON = (version: string): TransformPackageJsonFn =
         value = gen.next().value;
         updates.push(`  - ${depKey}: ${key}`);
       }
-      packageJSON[depKey] = deps;
+      packageJSON = updatePackageJSONForKey(packageJSON, depKey, deps)
     }
   }
   if (updates.length) {
