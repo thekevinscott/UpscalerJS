412757925c99b56e64f2c2f6e4aacaee51d2c5ca
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-07 21:30:13 -0400

    Fix esrgan legacy build

--- models/esrgan-legacy/src/gans.ts
+++ models/esrgan-legacy/src/gans.ts
@@ -1,24 +1,23 @@
-import { tf, } from './dependencies.generated';
+import { tfcore, tflayers } from './dependencies.generated';
 import { ModelDefinition } from '@upscalerjs/core';
 import { NAME, VERSION } from './constants.generated';
 
 const SCALE = 4;
 const BETA = 0.2;
 
-type Inputs = tf.Tensor4D | tf.Tensor4D[];
+type Inputs = tfcore.Tensor4D | tfcore.Tensor4D[];
 
-const isTensorArray = (inputs: Inputs): inputs is tf.Tensor4D[] => {
+const isTensorArray = (inputs: Inputs): inputs is tfcore.Tensor4D[] => {
   return Array.isArray(inputs);
 };
 
-const getInput = (inputs: Inputs): tf.Tensor4D => {
+const getInput = (inputs: Inputs): tfcore.Tensor4D => {
   if (isTensorArray(inputs)) {
     return inputs[0];
   }
   return inputs;
 };
-
-class MultiplyBeta extends tf.layers.Layer {
+class MultiplyBeta extends tflayers.layers.Layer {
   beta: number;
 
   constructor() {
@@ -27,13 +26,13 @@ class MultiplyBeta extends tf.layers.Layer {
   }
 
   call(inputs: Inputs) {
-    return tf.mul(getInput(inputs), this.beta);
+    return tfcore.mul(getInput(inputs), this.beta);
   }
 
   static className = 'MultiplyBeta';
 }
 
-class PixelShuffle extends tf.layers.Layer {
+class PixelShuffle extends tflayers.layers.Layer {
   scale: number;
 
   constructor() {
@@ -46,16 +45,23 @@ class PixelShuffle extends tf.layers.Layer {
   }
 
   call(inputs: Inputs) {
-    return tf.depthToSpace(getInput(inputs), this.scale, 'NHWC');
+    return tfcore.depthToSpace(getInput(inputs), this.scale, 'NHWC');
   }
 
   static className = 'PixelShuffle';
 }
 
+// TODO: For some reason, the tfjs-core package differs from an imported tfjs package,
+// in that tensor definitions do not have the clipByValue function. Therefore, we
+// manually set it.
+type ClipByValue = <T extends tfcore.Tensor>(min: number, max: number) => T;
+type TensorWithClipByValue = tfcore.Tensor & {
+  clipByValue: ClipByValue;
+}
 const modelDefinition: ModelDefinition = {
-  scale: 2,
+  scale: 4,
   channels: 3,
-  path: 'models/idealo/gans/model.json',
+  path: 'models/model.json',
   packageInformation: {
     name: NAME,
     version: VERSION,
@@ -63,11 +69,11 @@ const modelDefinition: ModelDefinition = {
   meta: {
     dataset: 'div2k',
   },
-  preprocess: (image: tf.Tensor) => tf.mul(image, 1 / 255),
-  postprocess: (output: tf.Tensor) => tf.tidy(() => {
-    const clippedValue = output.clipByValue(0, 1);
+  preprocess: (image: tfcore.Tensor) => tfcore.mul(image, 1 / 255),
+  postprocess: (output: tfcore.Tensor) => tfcore.tidy(() => {
+    const clippedValue = (output as TensorWithClipByValue).clipByValue(0, 1);
     output.dispose();
-    return tf.mul(clippedValue, 255);
+    return tfcore.mul(clippedValue, 255);
   }),
   customLayers: [MultiplyBeta, PixelShuffle,],
 };
