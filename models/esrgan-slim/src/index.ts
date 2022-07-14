997f3ca6b4201de83b8a600e6c03dccae37c78aa
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-13 21:10:07 -0400

    Only import types from node package

--- models/esrgan-slim/src/index.ts
+++ models/esrgan-slim/src/index.ts
@@ -1,6 +1,5 @@
 import * as tfc from '@tensorflow/tfjs-core';
-import * as _tf from '@tensorflow/tfjs-node';
-// import { tf as _tf, } from './dependencies.generated';
+import type * as _tf from '@tensorflow/tfjs-node';
 import type { ModelDefinition, } from '@upscalerjs/core';
 import { NAME, VERSION, } from './constants.generated';
 
@@ -79,79 +78,3 @@ const modelDefinition = (tf: typeof _tf) => {
 };
 
 export default modelDefinition;
-
-
-// import { tf, } from './dependencies.generated';
-// import type { ModelDefinition, } from '@upscalerjs/core';
-// import { NAME, VERSION, } from './constants.generated';
-
-// const SCALE = 4;
-// const BETA = 0.2;
-
-// type Inputs = tf.Tensor4D | tf.Tensor4D[];
-
-// const isTensorArray = (inputs: Inputs): inputs is tf.Tensor4D[] => {
-//   return Array.isArray(inputs);
-// };
-
-// const getInput = (inputs: Inputs): tf.Tensor4D => {
-//   if (isTensorArray(inputs)) {
-//     return inputs[0];
-//   }
-//   return inputs;
-// };
-// class MultiplyBeta extends tf.layers.Layer {
-//   beta: number;
-
-//   constructor() {
-//     super({});
-//     this.beta = BETA;
-//   }
-
-//   call(inputs: Inputs) {
-//     return tf.mul(getInput(inputs), this.beta);
-//   }
-
-//   static className = 'MultiplyBeta';
-// }
-
-// class PixelShuffle extends tf.layers.Layer {
-//   scale: number;
-
-//   constructor() {
-//     super({});
-//     this.scale = SCALE;
-//   }
-
-//   computeOutputShape(inputShape: number[]) {
-//     return [inputShape[0], inputShape[1], inputShape[2], 3,];
-//   }
-
-//   call(inputs: Inputs) {
-//     return tf.depthToSpace(getInput(inputs), this.scale, 'NHWC');
-//   }
-
-//   static className = 'PixelShuffle';
-// }
-
-// const modelDefinition: ModelDefinition = {
-//   scale: SCALE,
-//   channels: 3,
-//   path: 'models/model.json',
-//   packageInformation: {
-//     name: NAME,
-//     version: VERSION,
-//   },
-//   meta: {
-//     dataset: 'div2k',
-//   },
-//   preprocess: (image: tf.Tensor) => tf.mul(image, 1 / 255),
-//   postprocess: (output: tf.Tensor) => tf.tidy(() => {
-//     const clippedValue = (output).clipByValue(0, 1);
-//     output.dispose();
-//     return tf.mul(clippedValue, 255);
-//   }),
-//   customLayers: [MultiplyBeta, PixelShuffle,],
-// };
-
-// export default modelDefinition;
