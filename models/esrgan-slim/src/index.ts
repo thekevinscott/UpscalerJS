596c76a0d0154179ccb82fd056606b6c0d1e9fa5
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-13 20:42:21 -0400

    Go back to importing core

--- models/esrgan-slim/src/index.ts
+++ models/esrgan-slim/src/index.ts
@@ -1,79 +1,76 @@
-import * as tf from '@tensorflow/tfjs';
-import { ModelDefinitionObjectOrFn, ModelDefinitionFn, } from '@upscalerjs/core';
+import * as tf from '@tensorflow/tfjs-core';
+import * as tfl from '@tensorflow/tfjs-layers';
+import { ModelDefinitionObjectOrFn, } from '@upscalerjs/core';
 import { NAME, VERSION, } from './constants.generated';
 
-const main: ModelDefinitionFn = (_tf: typeof tf) => {
-  const SCALE = 4;
-  const BETA = 0.2;
+const SCALE = 4;
+const BETA = 0.2;
 
-  type Inputs = tf.Tensor4D | tf.Tensor4D[];
+type Inputs = tf.Tensor4D | tf.Tensor4D[];
 
-  const isTensorArray = (inputs: Inputs): inputs is tf.Tensor4D[] => {
-    return Array.isArray(inputs);
-  };
-
-  const getInput = (inputs: Inputs): tf.Tensor4D => {
-    if (isTensorArray(inputs)) {
-      return inputs[0];
-    }
-    return inputs;
-  };
-
-  class MultiplyBeta extends tf.layers.Layer {
-    beta: number;
-
-    constructor() {
-      super({});
-      this.beta = BETA;
-    }
-
-    call(inputs: Inputs) {
-      return _tf.mul(getInput(inputs), this.beta);
-    }
-
-    static className = 'MultiplyBeta';
-  }
-
-  class PixelShuffle extends tf.layers.Layer {
-    scale: number;
-
-    constructor() {
-      super({});
-      this.scale = SCALE;
-    }
-
-    computeOutputShape(inputShape: number[]) {
-      return [inputShape[0], inputShape[1], inputShape[2], 3,];
-    }
-
-    call(inputs: Inputs) {
-      return _tf.depthToSpace(getInput(inputs), this.scale, 'NHWC');
-    }
-
-    static className = 'PixelShuffle';
-  }
-
-  const modelDefinition: ModelDefinitionObjectOrFn = {
-    scale: SCALE,
-    channels: 3,
-    path: 'models/model.json',
-    packageInformation: {
-      name: NAME,
-      version: VERSION,
-    },
-    meta: {
-      dataset: 'div2k',
-    },
-    preprocess: (image: tf.Tensor) => _tf.mul(image, 1 / 255),
-    postprocess: (output: tf.Tensor) => _tf.tidy(() => {
-      const clippedValue = (output).clipByValue(0, 1);
-      output.dispose();
-      return _tf.mul(clippedValue, 255);
-    }),
-    customLayers: [MultiplyBeta, PixelShuffle,],
-  };
-
-  return modelDefinition;
+const isTensorArray = (inputs: Inputs): inputs is tf.Tensor4D[] => {
+  return Array.isArray(inputs);
 };
 
-export default main;
+const getInput = (inputs: Inputs): tf.Tensor4D => {
+  if (isTensorArray(inputs)) {
+    return inputs[0];
+  }
+  return inputs;
+};
+
+class MultiplyBeta extends tfl.layers.Layer {
+  beta: number;
+
+  constructor() {
+    super({});
+    this.beta = BETA;
+  }
+
+  call(inputs: Inputs) {
+    return tf.mul(getInput(inputs), this.beta);
+  }
+
+  static className = 'MultiplyBeta';
+}
+
+class PixelShuffle extends tfl.layers.Layer {
+  scale: number;
+
+  constructor() {
+    super({});
+    this.scale = SCALE;
+  }
+
+  computeOutputShape(inputShape: number[]) {
+    return [inputShape[0], inputShape[1], inputShape[2], 3,];
+  }
+
+  call(inputs: Inputs) {
+    return tf.depthToSpace(getInput(inputs), this.scale, 'NHWC');
+  }
+
+  static className = 'PixelShuffle';
+}
+
+const modelDefinition: ModelDefinitionObjectOrFn = {
+  scale: SCALE,
+  channels: 3,
+  path: 'models/model.json',
+  packageInformation: {
+    name: NAME,
+    version: VERSION,
+  },
+  meta: {
+    dataset: 'div2k',
+  },
+  preprocess: (image: tf.Tensor) => tf.mul(image, 1 / 255),
+  postprocess: (output: tf.Tensor) => tf.tidy(() => {
+    const clippedValue = (output).clipByValue(0, 1);
+    output.dispose();
+    return tf.mul(clippedValue, 255);
+  }),
+  customLayers: [MultiplyBeta, PixelShuffle,],
+};
+
+export default modelDefinition;
