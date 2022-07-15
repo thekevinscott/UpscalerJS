a56af6bd27d81ffa269e75995400d5ef20bab912
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-15 08:59:18 -0400

    Add index exports for models

---
+++ models/esrgan-legacy/src/index.ts
@@ -0,0 +1,5 @@
+export { default as GANS, } from './gans';
+export { default as PSNRSmall, } from './psnr-small';
+export { default as div2K2x, } from './div2k/2x';
+export { default as div2K3x, } from './div2k/3x';
+export { default as div2K4x, } from './div2k/4x';
