26b91da0cf02854381a3b09cc6041be9110878c3
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-18 19:58:54 -0400

    Fix tests path

--- test/jestconfig.browser.local.js
+++ test/jestconfig.browser.local.js
@@ -1,7 +1,9 @@
 const jestconfig = require('./jestconfig.json');
 module.exports = {
   ...jestconfig,
+  // "testRegex": "(/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "testRegex": "(^((?!.*(utils)).).*\.ts$)",
   roots: [
-    "<rootDir>/integration/browser/*.ts",
+    "<rootDir>/integration/browser",
   ],
 };
