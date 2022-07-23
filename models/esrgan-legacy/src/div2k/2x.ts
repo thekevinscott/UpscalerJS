df5e35fa6867d2c7df021ca9e9e8714266da7bb0
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-23 08:56:14 -0400

    Update 2x to clip

--- models/esrgan-legacy/src/div2k/2x.ts
+++ models/esrgan-legacy/src/div2k/2x.ts
@@ -1,16 +1,19 @@
-import type { Tensor4D, } from '@tensorflow/tfjs-core';
+import type { Tensor, Tensor3D, Tensor4D, } from '@tensorflow/tfjs-core';
 import type * as _tfng from '@tensorflow/tfjs-node-gpu';
 import type * as _tfn from '@tensorflow/tfjs-node';
 import type * as _tf from '@tensorflow/tfjs';
-
-type TF = typeof _tf | typeof _tfn | typeof _tfng;
-import { PostProcess, } from '@upscalerjs/core';
+import { PostProcess, ModelDefinitionFn, } from '@upscalerjs/core';
 import getModelDefinition from '../getModelDefinition';
 
-const postProcess: PostProcess = (output: Tensor4D) => tf.tidy(() => {
-  const clippedValue = (output).clipByValue(0, 255);
-  output.dispose();
-  return clippedValue;
-});
+type TF = typeof _tf | typeof _tfn | typeof _tfng;
+// TODO: Why do I need to specify TF here? Why can't it infer it from @upscaler/core's type definition?
+const modelDefinition: ModelDefinitionFn = (tf: TF) => {
+  const postProcess: PostProcess = (output: Tensor) => tf.tidy<Tensor3D>(() => {
+    const clippedValue = output.clipByValue(0, 255);
+    output.dispose();
+    return clippedValue as Tensor3D;
+  });
+  return getModelDefinition(2, 'div2k/2x', postProcess);
+};
 
-export default getModelDefinition(2, 'div2k/2x', postProcess);
+export default modelDefinition;
