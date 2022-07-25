aee0755f18fd38b3d4984a899bc11d9a94992bce
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-25 14:52:54 -0400

    Test imported TF from core (#397)

--- models/esrgan-legacy/src/div2k/2x.ts
+++ models/esrgan-legacy/src/div2k/2x.ts
@@ -1,11 +1,11 @@
 import type { Tensor, Tensor3D, } from '@tensorflow/tfjs-core';
-import type * as _tfng from '@tensorflow/tfjs-node-gpu';
-import type * as _tfn from '@tensorflow/tfjs-node';
-import type * as _tf from '@tensorflow/tfjs';
-import { PostProcess, ModelDefinitionFn, } from '@upscalerjs/core';
+// import type * as _tfng from '@tensorflow/tfjs-node-gpu';
+// import type * as _tfn from '@tensorflow/tfjs-node';
+// import type * as _tf from '@tensorflow/tfjs';
+import { PostProcess, ModelDefinitionFn, TF, } from '@upscalerjs/core';
 import getModelDefinition from '../getModelDefinition';
 
-type TF = typeof _tf | typeof _tfn | typeof _tfng;
+// type TF = typeof _tf | typeof _tfn | typeof _tfng;
 // TODO: Why do I need to specify TF here? Why can't it infer it from @upscaler/core's type definition?
 const modelDefinition: ModelDefinitionFn = (tf: TF) => {
   const postProcess: PostProcess = (output: Tensor) => tf.tidy<Tensor3D>(() => {
