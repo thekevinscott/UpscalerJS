d9907112c6bc08ae30d610278ed61ddcc4982213
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-25 15:34:46 -0400

    Refactor esrgan-legacy model definitions (#398)

--- models/esrgan-legacy/src/utils/getModelDefinition.ts
+++ models/esrgan-legacy/src/utils/getModelDefinition.ts
@@ -1,5 +1,5 @@
 import { ModelDefinition, PostProcess, } from '@upscalerjs/core';
-import { NAME, VERSION, } from './constants.generated';
+import { NAME, VERSION, } from '../constants.generated';
 
 const getModelDefinition = (scale: 2 | 3 | 4, modelPath: string, postprocess?: PostProcess): ModelDefinition => ({
   scale,
