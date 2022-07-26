5bab6093e1a5db76b3adf2e1923d455a1d9bc4fc
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-26 09:24:23 -0400

    Add platform tests

--- test/integration/node/platform.ts
+++ test/integration/node/platform.ts
@@ -1,74 +1,63 @@
+import path from 'path';
 import { checkImage } from '../../lib/utils/checkImage';
 import { prepareScriptBundleForNodeCJS, GetContents, testNodeScript } from '../../lib/node/prepare';
 import { LOCAL_UPSCALER_NAME } from '../../lib/node/constants';
-import { NodeTestRunner } from '../utils/NodeTestRunner';
+import { Main, NodeTestRunner } from '../utils/NodeTestRunner';
 
 const JEST_TIMEOUT = 60 * 1000;
 jest.setTimeout(JEST_TIMEOUT * 1); // 60 seconds timeout
 
-const writeScript = (deps: string): GetContents => outputFile => `
-${deps}
-const path = require('path');
-const fs = require('fs');
-const base64ArrayBuffer = require('../../utils/base64ArrayBuffer')
-
-const FIXTURES = path.join(__dirname, '../../../__fixtures__');
-const MODEL_PATH = path.join(FIXTURES, 'pixelator/pixelator.json');
-const TENSOR_PATH = path.join(FIXTURES, 'flower-small-tensor.json');
-
-// Returns a PNG-encoded UInt8Array
-const upscaleImageToUInt8Array = async (model, filename) => {
+const main: Main = async (deps) => {
+  const {
+    Upscaler,
+    tf,
+    base64ArrayBuffer,
+    flower,
+  } = deps;
   const upscaler = new Upscaler({
-    model: {
-      path: model,
-      scale: 4,
-    }
+    model: path.resolve(__dirname, '../../__fixtures__', 'pixelator/pixelator.json'),
   });
-  const bytes = new Uint8Array(JSON.parse(fs.readFileSync(filename, 'utf-8')));
+  const bytes = new Uint8Array(flower);
   const tensor = tf.tensor(bytes).reshape([16, 16, 3]);
-  return await upscaler.upscale(tensor, {
+  const result = await upscaler.upscale(tensor, {
     output: 'tensor',
     patchSize: 64,
-    padding: 6
+    padding: 6,
   });
-}
-
-const main = async (model) => {
-  const tensor = await upscaleImageToUInt8Array(model, TENSOR_PATH);
-  const upscaledImage = await tf.node.encodePng(tensor)
+  tensor.dispose();
+  // because we are requesting a tensor, it is possible that the tensor will
+  // contain out-of-bounds pixels; part of the value of this test is ensuring
+  // that those values are clipped in a post-process step.
+  const upscaledImage = await tf.node.encodePng(result);
+  result.dispose();
   return base64ArrayBuffer(upscaledImage);
-}
-
-const getModelPath = () => {
-  return 'file://' + path.resolve(MODEL_PATH);
-}
-
-(async () => {
-  const data = await main(getModelPath());
-  fs.writeFileSync('${outputFile}', data);
-})();
-`;
+};
 
 describe('Platform Integration Tests', () => {
   const testRunner = new NodeTestRunner({
+    main,
     trackTime: false,
+    dependencies: {
+      'fs': 'fs',
+      'base64ArrayBuffer': path.resolve(__dirname, '../../lib/utils/base64ArrayBuffer'),
+      'flower': path.resolve(__dirname, '../../__fixtures__', 'flower-small-tensor.json'),
+    },
   });
   beforeAll(async () => {
     await testRunner.beforeAll(prepareScriptBundleForNodeCJS);
   });
 
   [
-    { platform: 'node', deps: `
-const tf = require('@tensorflow/tfjs-node');
-const Upscaler = require('${LOCAL_UPSCALER_NAME}/node');
-    `},
-    { platform: 'node-gpu', deps: `
-const tf = require('@tensorflow/tfjs-node-gpu');
-const Upscaler = require('${LOCAL_UPSCALER_NAME}/node-gpu');
-    `},
-  ].forEach(({ platform, deps }) => {
+    { platform: 'node', },
+    { platform: 'node-gpu', },
+  ].forEach(({ platform, }) => {
     it(`loads a model with ${platform}`, async () => {
-      const result = await testNodeScript(writeScript(deps));
+      const result = await testRunner.test({
+        dependencies: {
+          'tf': `@tensorflow/tfjs-${platform}`,
+          'Upscaler': `${LOCAL_UPSCALER_NAME}/${platform}`,
+        },
+      });
       const formattedResult = `data:image/png;base64,${result}`;
       checkImage(formattedResult, "upscaled-4x-pixelator.png", 'diff.png');
     });
