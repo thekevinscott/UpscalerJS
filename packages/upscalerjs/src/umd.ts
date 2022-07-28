d70c7038e45f1b7e468c2ba910d737f0b46fb31e
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-28 15:45:08 -0400

    UMD export was faulty

--- packages/upscalerjs/src/umd.ts
+++ packages/upscalerjs/src/umd.ts
@@ -1,23 +1,20 @@
-// import { Upscaler, } from './upscaler';
-// import { AbortError, getRowsAndColumns, getTensorDimensions, } from './upscale';
+import { Upscaler, } from './upscaler';
+import { AbortError, getRowsAndColumns, getTensorDimensions, } from './upscale';
 
-// (
-//   Upscaler as typeof Upscaler & {
-//     getRowsAndColumns: typeof getRowsAndColumns;
-//   }
-// ).getRowsAndColumns = getRowsAndColumns;
-// (
-//   Upscaler as typeof Upscaler & {
-//     getTensorDimensions: typeof getTensorDimensions;
-//   }
-// ).getTensorDimensions = getTensorDimensions;
-// (
-//   Upscaler as typeof Upscaler & {
-//     AbortError: typeof AbortError;
-//   }
-// ).AbortError = AbortError;
+(
+  Upscaler as typeof Upscaler & {
+    getRowsAndColumns: typeof getRowsAndColumns;
+  }
+).getRowsAndColumns = getRowsAndColumns;
+(
+  Upscaler as typeof Upscaler & {
+    getTensorDimensions: typeof getTensorDimensions;
+  }
+).getTensorDimensions = getTensorDimensions;
+(
+  Upscaler as typeof Upscaler & {
+    AbortError: typeof AbortError;
+  }
+).AbortError = AbortError;
 
-class Upscaler {
-
-}
 export default Upscaler;
