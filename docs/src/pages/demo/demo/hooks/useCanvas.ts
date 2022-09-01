9763c7b8108255e56c6ce042a070c81431f32fc4
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-09-01 07:02:18 -0400

    Remove use of var for const

--- docs/src/pages/demo/demo/hooks/useCanvas.ts
+++ docs/src/pages/demo/demo/hooks/useCanvas.ts
@@ -3,12 +3,12 @@ import { spliceImage } from '../utils/spliceImage';
 
 const manipulateOriginalImage = (canvas: HTMLCanvasElement) => {
   const ctx = canvas.getContext('2d');
-  var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
   let min = Infinity;
-  for (var y = 0; y < imgPixels.height; y++) {
-    for (var x = 0; x < imgPixels.width; x++) {
-      var i = (y * 4) * imgPixels.width + x * 4;
-      var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3 / 2;
+  for (const y = 0; y < imgPixels.height; y++) {
+    for (const x = 0; x < imgPixels.width; x++) {
+      const i = (y * 4) * imgPixels.width + x * 4;
+      const avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3 / 2;
       if (avg < min) {
         min = avg;
       }
