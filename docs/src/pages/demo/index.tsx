c4008629bfb27c76b66b02103a63463cc1d7d2c6
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-09-02 09:44:08 -0400

    Fix the loading of the browser demo component

--- docs/src/pages/demo/index.tsx
+++ docs/src/pages/demo/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import Layout from '@theme/Layout';
-import Demo from '@site/src/components/demo/demo';
 
 export default function DemoPage() {
   return (
     <Layout title="Demo" description="A Demo of UpscalerJS">
       <BrowserOnly>
-        {() => <Demo />}
+        {() => {
+          const Demo = require('@site/src/components/demo/demo').Demo;
+          return (<Demo />);
+        }}
       </BrowserOnly>
     </Layout>
   );
