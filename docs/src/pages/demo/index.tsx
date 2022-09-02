fa16a1a57bd251dec85ad61a3c88cd8363e2ca50
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-09-02 09:39:53 -0400

    Wrap dmeo code in a browser only

--- docs/src/pages/demo/index.tsx
+++ docs/src/pages/demo/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import Layout from '@theme/Layout';
-import Demo from './demo/demo';
+import Demo from '@site/src/components/demo/demo';
 
 export default function DemoPage() {
   return (
     <Layout title="Demo" description="A Demo of UpscalerJS">
-      <Demo />
+      <BrowserOnly>
+        {() => <Demo />}
+      </BrowserOnly>
     </Layout>
   );
 }
