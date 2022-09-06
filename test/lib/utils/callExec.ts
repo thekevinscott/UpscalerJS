648088c03b417b931b64a2bc284d39ccd02fca78
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-09-06 08:03:37 -0400

    Add scripts to convert python model and create new model folder (#465)
    * Add scripts to convert python model and create new model folder
    
    * Update pnpm lock file

--- test/lib/utils/callExec.ts
+++ test/lib/utils/callExec.ts
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 type StdOut = (chunk: string) => void;
 type StdErr = (chunk: string) => void;
-const callExec = (cmd: string, options: any, stdout?: StdOut | boolean, stderr: StdErr | boolean = true): Promise<void> => new Promise((resolve, reject) => {
+const callExec = (cmd: string, options: any = {}, stdout?: StdOut | boolean, stderr: StdErr | boolean = true): Promise<void> => new Promise((resolve, reject) => {
   const spawnedProcess = exec(cmd, options, (error: Error) => {
     if (error) {
       reject(error);
