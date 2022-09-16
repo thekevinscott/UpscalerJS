612107571406dcf72519a248b9a2d9631503137c
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-09-16 07:42:55 -0400

    Update the browser examples to use Stackblitz

--- test/lib/utils/callExec.ts
+++ test/lib/utils/callExec.ts
@@ -1,4 +1,4 @@
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
 type StdOut = (chunk: string) => void;
 type StdErr = (chunk: string) => void;
 const callExec = (cmd: string, options: any = {}, stdout?: StdOut | boolean, stderr: StdErr | boolean = true): Promise<void> => new Promise((resolve, reject) => {
@@ -23,5 +23,27 @@ const callExec = (cmd: string, options: any = {}, stdout?: StdOut | boolean, std
   } else {
     spawnedProcess.stdout.pipe(process.stdout);
   }
-})
+
+  return spawnedProcess;
+});
 export default callExec;
+
+// export const runProcess = (command: string) => {
+//   const spawnedProcess = spawn(command, { shell: true });
+
+//   spawnedProcess.stdout.on('data', (data: any) => {
+//     process.stdout.write(data.toString());
+//   });
+
+//   spawnedProcess.stderr.on('data', (data: any) => {
+//     process.stderr.write(data.toString());
+//   });
+
+//   spawnedProcess.on('exit', (data: any) => {
+//     if (data) {
+//       process.stderr.write(data.toString());
+//     }
+//   });
+
+//   return spawnedProcess;
+// };
