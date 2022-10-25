9fdf7e3176a5c3e1794f61ab72e0c849e16c9bb0
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-10-25 07:24:45 -0400

    Move jest setup to typescript (#550)
    * Move jest setup to typescript
    
    * Fix jest config
    
    * Fix jest config path

--- test/misc/memory/jestconfig.js
+++ test/misc/memory/jestconfig.js
@@ -1,14 +1,11 @@
-// const jestconfig = require('./jestconfig');
-// module.exports = {
-//   ...jestconfig,
-//   "testRunner": "jest-circus/runner",
-//   "preset": "ts-jest",
-//   "testRegex": "test.browser.js",
-// }
+const path = require('path');
+const ROOT = path.resolve(__dirname, '../../..');
+const TEST_ROOT = path.resolve(ROOT, 'test')
 
-const jestconfig = require('../../jestconfig.json');
+const jestconfig = require(path.resolve(TEST_ROOT, 'jestconfig.json'));
 module.exports = {
   ...jestconfig,
+  "setupFilesAfterEnv": [path.resolve(TEST_ROOT, "jest.setup.ts")],
   setupFiles: [],
   roots: [
     "<rootDir>",
