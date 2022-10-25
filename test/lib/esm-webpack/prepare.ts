758a025b0dc74e5646097cb8588b6f8c92b2cb1c
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-10-25 05:33:29 -0400

    Generate the test server index files dynamically

--- test/lib/esm-webpack/prepare.ts
+++ test/lib/esm-webpack/prepare.ts
@@ -3,7 +3,7 @@ import rimraf from 'rimraf';
 import { copyFixtures } from '../utils/copyFixtures';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { installLocalPackages, installNodeModules } from '../shared/prepare';
+import { installLocalPackages, installNodeModules, writeIndex } from '../shared/prepare';
 import { LOCAL_UPSCALER_NAME } from './constants';
 import { MockCDN } from '../../integration/utils/BrowserTestRunner';
 
@@ -25,12 +25,22 @@ export const bundleWebpack = (): Promise<void> => new Promise(async (resolve, re
   rimraf.sync(DIST);
   copyFixtures(DIST);
 
-  const entryFiles = path.join(ROOT, 'src/index.js');
+  const entryFile = path.join(ROOT, 'src/index.js');
+  writeIndex(entryFile, `
+import * as tf from '@tensorflow/tfjs';
+import Upscaler from 'upscaler-for-esm-webpack';
+import flower from '../../../__fixtures__/flower-small.png';
+window.tfjs = tf;
+window.flower = flower;
+window.Upscaler = Upscaler;
+document.title = document.title + ' | Loaded';
+document.body.querySelector('#output').innerHTML = document.title;
+  `);
 
   const compiler = webpack({
     mode: 'production',
     context: ROOT,
-    entry: entryFiles,
+    entry: entryFile,
     stats: 'errors-only',
     plugins: [new HtmlWebpackPlugin({
       title: 'UpscalerJS Integration Test: ESM via Webpack',
