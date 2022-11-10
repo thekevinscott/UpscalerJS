df51872d9ea0e7a89ffb4dbdf73f303af1168395
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-11-10 05:31:52 -0500

    Refactor copy fixtures script to accept options and populate local models

--- test/lib/utils/copyFixtures.ts
+++ test/lib/utils/copyFixtures.ts
@@ -1,13 +1,28 @@
 import fs from 'fs';
+import { copySync } from 'fs-extra';
 import path from 'path';
+import { getAllAvailableModelPackages } from '../../../scripts/package-scripts/utils/getAllAvailableModels';
 
-const ROOT = __dirname;
-const FIXTURES = path.join(ROOT, '../../__fixtures__');
-export const copyFixtures = (dist: string, includeFlower: boolean = true) => {
+const ROOT = path.resolve(__dirname, '../../../');
+const MODELS = path.resolve(ROOT, 'models');
+const FIXTURES = path.resolve(ROOT, 'test/__fixtures__');
+interface CopyFixtureOpts {
+  includeFixtures?: boolean;
+  includeModels?: boolean;
+}
+
+export const copyFixtures = (dist: string, { includeFixtures = true, includeModels = false }: CopyFixtureOpts = {} ) => {
   fs.mkdirSync(path.join(dist, 'pixelator'), { recursive: true });
   fs.copyFileSync(path.join(FIXTURES, 'pixelator/pixelator.json'), path.join(dist, 'pixelator/pixelator.json'))
   fs.copyFileSync(path.join(FIXTURES, 'pixelator/pixelator.weights.bin'), path.join(dist, 'pixelator/pixelator.weights.bin'))
-  if (includeFlower) {
+  if (includeFixtures) {
     fs.copyFileSync(path.join(FIXTURES, 'flower-small.png'), path.join(dist, 'flower-small.png'))
+  }
+  if (includeModels) {
+    getAllAvailableModelPackages().map(packageName => {
+      const srcDir = path.resolve(MODELS, packageName, 'models');
+      const destDir = path.resolve(dist, 'models', packageName, 'models');
+      copySync(srcDir, destDir, { overwrite: true });
+    });
   }
 };
