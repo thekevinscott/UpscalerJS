55e937971029fc808319b1baa1ba4d535e02df37
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-11-11 07:33:08 -0500

    Fix missing definitions

--- scripts/package-scripts/utils/constants.ts
+++ scripts/package-scripts/utils/constants.ts
@@ -6,7 +6,9 @@ export const EXAMPLES_DIR = path.resolve(ROOT_DIR, 'examples');
 export const TEST_DIR = path.resolve(ROOT_DIR, 'test');
 export const DOCS_DIR = path.resolve(ROOT_DIR, 'docs');
 export const TMP_DIR = path.resolve(ROOT_DIR, 'tmp');
+export const DEV_DIR = path.resolve(ROOT_DIR, 'dev');
 export const PACKAGES_DIR = path.resolve(ROOT_DIR, 'packages');
 
 export const UPSCALER_DIR = path.resolve(PACKAGES_DIR, 'upscalerjs');
 export const CORE_DIR = path.resolve(PACKAGES_DIR, 'core');
+export const WRAPPER_DIR = path.resolve(PACKAGES_DIR, 'upscalerjs-wrapper');
