16a6583fa6c9cfc769e4a699396dda5c4af9ac9d
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-11-11 08:26:13 -0500

    Fix benchmarking bugs (#601)
    * Fix bug with incorrect table
    
    * Need to await database initialization

--- scripts/package-scripts/benchmark/performance/utils/sequelize.ts
+++ scripts/package-scripts/benchmark/performance/utils/sequelize.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { Sequelize } from 'sequelize';
 import { TMP_DIR } from '../../../utils/constants';
 
-const DATABASE_FILE = path.resolve(TMP_DIR, 'performance.sql');
+const DATABASE_FILE = path.resolve(TMP_DIR, 'cache.sql');
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: DATABASE_FILE,
