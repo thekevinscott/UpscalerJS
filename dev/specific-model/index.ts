d28fcae240e20ffd84665c9afa05b8a3c65bd445
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-11-13 12:15:13 -0500

    Update dev files and move gitignore definitions around (#607)

---
+++ dev/specific-model/index.ts
@@ -0,0 +1,49 @@
+import Upscaler from '../../packages/upscalerjs/src/index';
+import model from '../../models/esrgan-thick/src/8x';
+import flower from './flower.png';
+import * as tf from '@tensorflow/tfjs';
+
+const status = document.getElementById('status')!;
+
+const getModel = (path: string) => {
+  const { packageInformation, ...rest } = model(tf);
+  return {
+    ...rest,
+    path: path,
+  };
+}
+
+const makeImg = (path: string, label: string) => {
+  const img = new Image();
+  img.src = path;
+  img.onload = () => {
+    const divEl = document.createElement('div');
+    const imgEl = document.createElement('img');
+    const labelEl = document.createElement('label');
+    labelEl.innerText = label;
+    imgEl.src = path;
+    imgEl.width = img.width;
+    imgEl.height = img.height;
+    imgEl.appendChild(img);
+
+    divEl.appendChild(labelEl);
+    divEl.appendChild(imgEl);
+    divEl.appendChild(document.createElement('hr'));
+
+    document.body.appendChild(divEl);
+  }
+}
+
+(async () => {
+  makeImg(flower, 'Original');
+  const model = getModel('/models/esrgan-thick/models/8x/model.json');
+  status.innerHTML = 'Starting';
+  const upscaler = new Upscaler({
+    model,
+  });
+  status.innerHTML = 'Upscaling...';
+  const upscaledImg = await upscaler.upscale(flower);
+  status.innerHTML = 'Image upscaled';
+  makeImg(upscaledImg, 'Upscaled');
+  status.innerHTML = 'Image printed';
+})();
