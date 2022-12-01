2c8ed1b3b657d5fb2efaf1eddf51729ddfd4ab24
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-12-01 07:07:18 -0500

    Add sidebars and remove Models from header

--- docs/docusaurus.config.cjs
+++ docs/docusaurus.config.cjs
@@ -88,11 +88,11 @@ const config = {
             position: 'left',
             label: 'Demo',
           },
-          {
-            to: '/models',
-            position: 'left',
-            label: 'Models',
-          },
+          // {
+          //   to: '/models',
+          //   position: 'left',
+          //   label: 'Models',
+          // },
           // {
           //   type: 'doc',
           //   docId: 'intro',
