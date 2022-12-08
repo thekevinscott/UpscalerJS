eff60835b526e4822a38106cf7e1b01b4ca4c32e
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-12-07 19:05:47 -0500

    Fix demo import

--- docs/src/components/demo/hooks/useUpscaler.ts
+++ docs/src/components/demo/hooks/useUpscaler.ts
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import { useCanvas } from './useCanvas';
 import { ReceiverWorkerState, SenderWorkerState } from './worker';
+import { tensorAsBase64 } from 'tensor-as-base64';
 
 // TODO: This has to manually be set to the scale of the model loaded in the worker.
 const SCALE = 4;
@@ -13,15 +14,62 @@ export const useUpscaler = (img?: HTMLImageElement) => {
 
   const upscaledSrc = useRef<string>();
 
-  const { createCanvas } = useCanvas(SCALE);
+  const { drawImage, createCanvas } = useCanvas(SCALE);
 
   const setUpscaledSrc = useCallback((newSrc: string) => {
     upscaledSrc.current = newSrc;
   }, []);
 
-  const [patchSize] = useState<number>(PATCH_SIZE);
-
   const worker = useRef<Worker>();
+
+  useEffect(() => {
+    worker.current = new Worker(new URL('worker.ts', import.meta.url));
+    worker.current.postMessage({
+      type: ReceiverWorkerState.INSTANTIATE,
+    });
+
+    return () => {
+      worker.current.terminate();
+    }
+  }, []);
+
+  useEffect(() => {
+    worker.current.postMessage({
+      type: ReceiverWorkerState.SET_ID,
+      data: {
+        id: img?.src,
+      }
+    });
+  }, [img]);
+
+  useEffect(() => {
+    worker.current.onmessage = async ({ data: { type, data } }) => {
+      if (type === SenderWorkerState.PROGRESS) {
+        if (upscaling) {
+          const {
+            id,
+            rate,
+            row,
+            col,
+            slice,
+            shape,
+          } = data;
+          if (id === img.src) {
+            setProgress(rate);
+            const tensor = tf.tensor3d(slice, shape);
+            const dataURL = await tensorAsBase64(tensor);
+            tensor.dispose();
+            setUpscaledSrc(await drawImage(dataURL, PATCH_SIZE * SCALE, col, row));
+            if (rate === 1) {
+              setUpscaling(false);
+            }
+          }
+        } else {
+          console.warn('Received progress event, but we are not supposed to be upscaling.');
+        }
+      }
+    }
+  }, [upscaling, img, setProgress, setUpscaledSrc,]);
 
   const upscale = useCallback(async (img: HTMLImageElement) => {
     setUpscaledSrc(createCanvas(img));
@@ -34,11 +82,11 @@ export const useUpscaler = (img?: HTMLImageElement) => {
       data: {
         pixels: src.dataSync(),
         shape: src.shape,
-        patchSize,
+        patchSize: PATCH_SIZE,
         padding: 2,
       }
     });
-  }, [patchSize, createCanvas, setUpscaling, setProgress]);
+  }, [createCanvas, setUpscaling, setProgress]);
 
   const cancelUpscale = useCallback(() => {
     worker.current.postMessage({
