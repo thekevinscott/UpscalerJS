9313cb1f0f34da625f9847313e8039bbe926c81b
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-12-13 20:18:42 -0500

    Bump timing on tests

--- test/integration/node/speed.ts
+++ test/integration/node/speed.ts
@@ -76,7 +76,7 @@ describe('Node Speed Integration Tests', () => {
     await testRunner.beforeAll(prepareScriptBundleForNodeCJS);
   }, 1000 * 120);
 
-  if (new Date().getTime() > 1670772364086) {
+  if (new Date().getTime() > 1673364364086) {
     [
       {
         label: 'Simple Model',
