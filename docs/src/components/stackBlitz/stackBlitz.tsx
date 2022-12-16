7ecf26dc2b081377913ccb5fef4f046d5e038cbf
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-12-16 17:49:37 -0500

    Add an embed component when specified in markdown

--- docs/src/components/stackBlitz/stackBlitz.tsx
+++ docs/src/components/stackBlitz/stackBlitz.tsx
@@ -1,7 +1,11 @@
-import React, { useMemo } from 'react';
+import React, { MouseEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
+import styles from './stackBlitz.module.scss';
+import clsx from 'clsx';
 
 const ROOT_URL = 'https://stackblitz.com/github/thekevinscott/upscalerjs/tree/main';
+const THRESHOLD_TO_GO_MAX = 100;
+const HEADER_HEIGHT = 60;
 
 const getParamsWithColorMode = (params: URLSearchParams | string, colorMode: string) => {
   if (typeof params === 'string') {
@@ -12,8 +16,27 @@ const getParamsWithColorMode = (params: URLSearchParams | string, colorMode: str
   return params.toString();
 }
 
-export const StackBlitz = ({ url, params = new URLSearchParams(), height= '400', width = '100%'}: { url: string, params?: URLSearchParams | string, height?: string, width?: string }) => {
+export const StackBlitz = ({
+  url,
+  params = new URLSearchParams(),
+  persist,
+}: {
+  url: string,
+  params?: URLSearchParams | string,
+  persist?: string;
+}) => {
+  const ref = useRef<HTMLIFrameElement>(null);
   const { colorMode } = useColorMode();
+  const [height, setHeight] = useState(300);
+  const [delta, setDelta] = useState(0);
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    if (dragging === false) {
+      setHeight(height + delta);
+      setDelta(0);
+    }
+  }, [dragging]);
 
   const src = useMemo(() => {
     const builtURL = [
@@ -27,9 +50,78 @@ export const StackBlitz = ({ url, params = new URLSearchParams(), height= '400',
     colorMode,
   ]);
 
+  let containerHeight: number | string = height + delta;
+  if (window?.visualViewport && window.visualViewport.height - HEADER_HEIGHT < containerHeight) {
+    containerHeight = window.visualViewport.height - HEADER_HEIGHT;
+    // containerHeight = `calc(100vh - ${HEADER_HEIGHT}px - 20px)`;
+  }
+
+  if (persist) {
+    return (
+      <div className={styles.container} style={{ height: containerHeight }}>
+        {dragging && <div className={styles.overlay}></div>}
+        <iframe className={styles.iframe} ref={ref} src={src}></iframe>
+        <Dragger onDragging={setDragging} onDrag={setDelta} />
+      </div>
+    );
+  }
+
   return (
-    <iframe height={height} width={width} src={src}></iframe>
+    <iframe className={styles.iframe} ref={ref} src={src}></iframe>
   )
+}
+
+const Dragger = ({
+  onDrag,
+  onDragging,
+}: {
+  onDrag: (delta: number) => void;
+  onDragging: (dragging: boolean) => void;
+}) => {
+  const [start, setStart] = useState<number>(0);
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    const drag = (e: any) => {
+      if (dragging) {
+        const drag = e.clientY - start
+        console.log('drag', drag);
+        onDrag(drag);
+      }
+    }
+
+    window.addEventListener('mousemove', drag);
+    return () => {
+      window.removeEventListener('mousemove', drag);
+    }
+  }, [dragging, start, onDrag]);
+
+  useEffect(() => {
+    const mouseup = () => {
+      setDragging(false);
+    };
+
+    window.addEventListener('mouseup', mouseup);
+    return () => {
+      window.removeEventListener('mouseup', mouseup);
+    }
+  }, []);
+
+  const startDrag = useCallback((e: MouseEvent) => {
+    setStart(e.clientY);
+    setDragging(true);
+  }, []);
+
+  const stopDrag = useCallback(() => {
+  }, []);
+
+  useEffect(() => {
+    onDragging(dragging);
+  }, [dragging]);
+
+  return (
+    <small onMouseDown={startDrag}>Drag to resize</small>
+  );
 }
 
 export default StackBlitz;
--- docs/src/plugins/remark/section-prefix/index.js
+++ docs/src/plugins/remark/section-prefix/index.js
@@ -2,20 +2,27 @@ const visit = require('unist-util-visit');
 
 const plugin = (options) => {
   const transformer = async (ast) => {
-    const s = JSON.stringify(ast);
-    if (s.includes('ooh')) {
-      console.log('ast', JSON.stringify(ast, null, 2));
-    }
-    let number = 1;
-    visit(ast, 'heading', (node) => {
-      if (node.depth === 2 && node.children.length > 0) {
-        node.children.unshift({
-          type: 'text',
-          value: `Section ${number}. `,
-        });
-        number++;
-      }
-    });
+    // console.log('ast', JSON.stringify(ast, null, 2));
+    // let number = 1;
+    // ast.children.unshift({
+    //   type: 'text',
+    //   value: 'WHAT UP BIG BOY'
+    // })
+    // ast.children.unshift({
+    //   "type": "jsx",
+    //   "value": "<StackBlitz persist=\"1\" url=\"/examples/basic\" params=\"embed=1&file=index.js&hideExplorer=1\" />",
+    // });
+    // const header = ast.children.shift();
+    // ast.children.unshift(header);
+    // visit(ast, 'heading', (node) => {
+    //   if (node.depth === 2 && node.children.length > 0) {
+    //     node.children.unshift({
+    //       type: 'text',
+    //       value: `Section ${number}. `,
+    //     });
+    //     number++;
+    //   }
+    // });
   };
   return transformer;
 };
