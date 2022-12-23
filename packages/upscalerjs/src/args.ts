1442946dd47bed2e1e21669656feddddc17f46fc
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-12-23 08:37:14 -0500

    Merge upstream

--- packages/upscalerjs/src/args.ts
+++ packages/upscalerjs/src/args.ts
@@ -1,4 +1,4 @@
-import { BASE64, Progress, ResultFormat, UpscaleArgs as PrivateUpscaleArgs, UpscaleArgs as PublicUpscaleArgs, } from "./types";
+import { BASE64, Progress, ResultFormat, PrivateUpscaleArgs, PublicUpscaleArgs, } from "./types";
 
 export function getUpscaleOptions<P extends Progress<O, PO>, O extends ResultFormat = BASE64, PO extends ResultFormat = undefined>(options: PublicUpscaleArgs<P, O, PO>): PrivateUpscaleArgs<P, O, PO> {
   return options;
