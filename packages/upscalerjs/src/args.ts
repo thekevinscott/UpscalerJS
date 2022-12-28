3dcc88fd5489eca761681dc9f61743d3053bd1dc
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-12-28 08:30:45 -0500

    Fix deepsource issue

--- packages/upscalerjs/src/args.ts
+++ packages/upscalerjs/src/args.ts
@@ -1,5 +1,12 @@
 import { BASE64, PrivateUpscaleArgs, TENSOR, } from "./types";
 
+const getOutputOption = (output?: unknown): TENSOR | BASE64 => {
+  if (output === 'tensor') {
+    return 'tensor';
+  }
+  return 'base64';
+};
+
 export function getUpscaleOptions(options: Omit<PrivateUpscaleArgs, 'output' | 'progressOutput'> & { output?: unknown; progressOutput?: unknown } = {}): PrivateUpscaleArgs {
   return {
     ...options,
@@ -7,10 +14,3 @@ export function getUpscaleOptions(options: Omit<PrivateUpscaleArgs, 'output' | '
     progressOutput: getOutputOption(options.progressOutput || options.output),
   };
 }
-
-const getOutputOption = (output?: unknown): TENSOR | BASE64 => {
-  if (output === 'tensor') {
-    return 'tensor';
-  }
-  return 'base64';
-};
