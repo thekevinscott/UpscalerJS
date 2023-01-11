57568603e4d95b5599bf748e820be717020fecdc
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-01-10 19:15:51 -0500

    Working implementation of a Cloudflare Worker

---
+++ examples/cloudflare-worker/src/worker/index.js
@@ -0,0 +1,64 @@
+/*****
+ * Fetch images from the Pixabay API
+ */
+
+import * as tf from '@tensorflow/tfjs';
+import Upscaler from 'upscaler';
+// import model from '@upscalerjs/esrgan-slim/4x';
+import model from '@upscalerjs/esrgan-thick/4x';
+
+tf.setBackend('cpu');
+
+const upscale = async (input) => {
+  await tf.ready();
+  
+  setTimeout(() => {
+    throw new Error('Waited 29.9s to load, some error. Explicitly thrown for visibility. Free tier wall time is 30s');
+  }, (1000 * 30) - 100);
+
+  console.log('Instantiating upscaler...');
+  const upscaler = new Upscaler({
+    model,
+  });
+  await upscaler.getModel(); // if there's an error loading the model, surface it
+
+  console.log('Instantiated upscaler successfully');
+  const upscaledSrc = await upscaler.upscale(input, {
+    output: 'tensor',
+  });
+
+  console.log('Got upscaled response');
+  const response = {
+    data: Array.from(upscaledSrc.dataSync()),
+    shape: upscaledSrc.shape,
+  };
+  upscaledSrc.dispose();
+  return response;
+};
+
+async function handleRequest(request) {
+  const responseHeaders = new Headers();
+  responseHeaders.set('content-type', 'application/json;charset=UTF-8');
+  responseHeaders.set('Access-Control-Allow-Origin', '*')
+  const init = {
+    headers: responseHeaders,
+  };
+  try {
+    const { data, shape } = await request.json();
+    console.log('Received request with shape', shape);
+    const tensor = tf.tensor(data, shape);
+    console.log('Built tensor');
+    const response = await upscale(tensor);
+    console.log('Upscaled image');
+    tensor.dispose();
+    return new Response(JSON.stringify(response), init);
+  } catch (err) {
+    console.error('The upscale function threw an error');
+    console.error(err.message);
+    return new Response(JSON.stringify({ err: err.message }), init);
+  }
+}
+
+addEventListener('fetch', event => {
+  return event.respondWith(handleRequest(event.request));
+});
--- packages/upscalerjs/src/loadModel.browser.ts
+++ packages/upscalerjs/src/loadModel.browser.ts
@@ -11,6 +11,8 @@ type CDN = 'jsdelivr' | 'unpkg';
 
 type CdnFn = (packageName: string, version: string, path: string) => string;
 
+type Errors = [CDN, Error][];
+
 export const CDN_PATH_DEFINITIONS: { [key in CDN]: CdnFn } = {
   'jsdelivr': (packageName, version, path) => `https://cdn.jsdelivr.net/npm/${packageName}@${version}/${path}`,
   'unpkg': (packageName, version, path) => `https://unpkg.com/${packageName}@${version}/${path}`,
@@ -21,21 +23,27 @@ export const CDNS: CDN[] = [
   'unpkg',
 ];
 
-export const getLoadModelErrorMessage = (modelPath: string): Error => new Error(`Could not resolve URL ${modelPath}`);
+export const getLoadModelErrorMessage = (modelPath: string, packageInformation: PackageInformation, errs: Errors): Error => new Error([
+  `Could not resolve URL ${modelPath} for package ${packageInformation.name}@${packageInformation.version}`,
+  `Errors include:`,
+  ...errs.map(([cdn, err]) => `- ${cdn}: ${err.message}`),
+].join('\n'));
 
 export const fetchModel = async (modelPath: string, packageInformation?: PackageInformation): Promise<tf.LayersModel> => {
   if (packageInformation) {
+    const errs: Errors = [];
     for (let i = 0; i < CDNS.length; i++) {
       const cdn = CDNS[i];
       const getCDNFn = CDN_PATH_DEFINITIONS[cdn];
       try {
         const url = getCDNFn(packageInformation.name, packageInformation.version, modelPath);
         return await tf.loadLayersModel(url);
-      } catch (err) {
+      } catch (err: unknown) {
         // there was an issue with the CDN, try another
+        errs.push([cdn, err instanceof Error ? err : new Error(`There was an unknown error: ${JSON.stringify(err)}`)]);
       }
     }
-    throw getLoadModelErrorMessage(modelPath);
+    throw getLoadModelErrorMessage(modelPath, packageInformation, errs);
   }
   return await tf.loadLayersModel(modelPath);
 };
