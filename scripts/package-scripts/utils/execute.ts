2d71918206407678e76de0fd9f942276a7fdcbe6
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-01-18 08:15:13 -0500

    Add an optional cwd option to the execute command (#788)

--- scripts/package-scripts/utils/execute.ts
+++ scripts/package-scripts/utils/execute.ts
@@ -1,6 +1,8 @@
 import { exec } from 'child_process';
-const execute = (cmd: string): Promise<void> => new Promise((resolve, reject) => {
-  const spawnedProcess = exec(cmd, {}, (error: Error | null) => {
+const execute = (cmd: string, { cwd, }: { cwd?: string } = {}): Promise<void> => new Promise((resolve, reject) => {
+  const spawnedProcess = exec(cmd, {
+    cwd,
+  }, (error: Error | null) => {
     if (error) {
       reject(error.message);
     } else {
