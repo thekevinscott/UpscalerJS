193cf41f7687f356266bd738909dcf488b5d26dd
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-01-19 08:07:42 -0500

    Move tensor functions to core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
 import type * as tf from '@tensorflow/tfjs';
 import type * as tfNode from '@tensorflow/tfjs-node';
 import type * as tfNodeGpu from '@tensorflow/tfjs-node-gpu';
-import type { Tensor, Tensor4D, serialization, } from '@tensorflow/tfjs-core';
+import type { Tensor, Tensor3D, Tensor4D, serialization, } from '@tensorflow/tfjs-core';
 
 export type TF = typeof tf | typeof tfNode | typeof tfNodeGpu;
 
@@ -15,6 +15,7 @@ type CustomLayer = Parameters<typeof serialization.registerClass>[0];
 
 type MetaValue = string | number | Meta | null | undefined | boolean;
 export type Meta = { [key: string]: MetaValue };
+export type ModelType = 'graph' | 'layers';
 
 export type PreProcess = ProcessFn<Tensor4D>;
 export type PostProcess = ProcessFn<Tensor4D>;
@@ -25,7 +26,7 @@ export interface ModelDefinition {
    */
   path: string;
   /**
-   * The scale of the model. Must match the scale at which the model was trained.
+   * The scale of the model. For super resolution models, should match the scale at which the model was trained.
    */
   scale: number;
   /**
@@ -52,7 +53,6 @@ export interface ModelDefinition {
    * Custom layers for the model. You can learn more about custom layers [here](https://www.tensorflow.org/js/guide/models_and_layers#custom_layers).
    */
   customLayers?: CustomLayer[];
-
   /**
    * @hidden
    */
@@ -62,3 +62,17 @@ export interface ModelDefinition {
 export type ModelDefinitionFn = (tf: TF) => ModelDefinition;
 
 export type ModelDefinitionObjectOrFn = ModelDefinitionFn | ModelDefinition;
+
+export type IsTensor<T extends tf.Tensor> = (pixels: Tensor) => pixels is T;
+export function makeIsNDimensionalTensor<T extends Tensor>(rank: number): IsTensor<T> {
+  function fn(pixels: Tensor): pixels is T {
+    try {
+      return pixels.shape.length === rank;
+    } catch (err) { }
+    return false;
+  }
+
+  return fn;
+}
+export const isFourDimensionalTensor = makeIsNDimensionalTensor<Tensor4D>(4);
+export const isThreeDimensionalTensor = makeIsNDimensionalTensor<Tensor3D>(3);
