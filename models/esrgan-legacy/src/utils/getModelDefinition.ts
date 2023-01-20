179b4bdce1af484812965bd808486be6ccb21561
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-01-20 08:21:04 -0500

    Remove channel information from model definitions (#796)

--- models/esrgan-legacy/src/utils/getModelDefinition.ts
+++ models/esrgan-legacy/src/utils/getModelDefinition.ts
@@ -3,7 +3,6 @@ import { NAME, VERSION, } from '../constants.generated';
 
 const getModelDefinition = (scale: 2 | 3 | 4, modelPath: string, postprocess?: PostProcess): ModelDefinition => ({
   scale,
-  channels: 3,
   path: `models/${modelPath}/model.json`,
   packageInformation: {
     name: NAME,
