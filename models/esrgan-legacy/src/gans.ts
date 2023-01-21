0eb81502512e516bc23ed6d0e91aa4f7f8b73f1a
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-01-21 09:02:10 -0500

    Switch GANs model to leverage input range

--- models/esrgan-legacy/src/gans.ts
+++ models/esrgan-legacy/src/gans.ts
@@ -63,7 +63,7 @@ const modelDefinition: ModelDefinitionFn = (tf: TF) => {
     meta: {
       dataset: 'div2k',
     },
-    preprocess: (image: Tensor) => tf.mul(image, 1 / 255),
+    inputRange: [0, 1,],
     postprocess: (output: Tensor) => tf.tidy(() => {
       const clippedValue = (output).clipByValue(0, 1);
       output.dispose();
