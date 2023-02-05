3acef9b36a2a08e5d8861093d20d8b3026a71272
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-05 16:14:40 -0500

    Fix tsconfig configs

--- models/default-model/src/index.ts
+++ models/default-model/src/index.ts
@@ -1,29 +1,28 @@
+import { ModelDefinitionFn, } from '@upscalerjs/core';
 import { getESRGANModelDefinition, } from '@upscalerjs/shared/esrgan';
 import { NAME, VERSION, } from './constants.generated';
 
-console.log(getESRGANModelDefinition, NAME, VERSION);
-
 const SCALE = 2;
 
-export default SCALE;
+const modelDefinition: ModelDefinitionFn = getESRGANModelDefinition({
+  scale: SCALE,
+  name: NAME,
+  version: VERSION,
+  path: `models/model.json`,
+  meta: {
+    C: 1,
+    D: 2,
+    G: 4,
+    G0: 64,
+    T: 10,
+    architecture: "rdn",
+    patchSize: 128,
+    size: 'slim',
+    artifactReducing: false,
+    sharpening: false,
+    dataset: 'div2k',
+    modelFileName: 'rdn-C1-D2-G4-G064-T10-x2-patchsize128-compress100-sharpen0-datadiv2k-vary_cFalse_best-val_loss_epoch494',
+  },
+});
 
-// export default getESRGANModelDefinition({
-//   scale: SCALE,
-//   name: NAME,
-//   version: VERSION,
-//   path: `models/model.json`,
-//   meta: {
-//     C: 1,
-//     D: 2,
-//     G: 4,
-//     G0: 64,
-//     T: 10,
-//     architecture: "rdn",
-//     patchSize: 128,
-//     size: 'slim',
-//     artifactReducing: false,
-//     sharpening: false,
-//     dataset: 'div2k',
-//     modelFileName: 'rdn-C1-D2-G4-G064-T10-x2-patchsize128-compress100-sharpen0-datadiv2k-vary_cFalse_best-val_loss_epoch494',
-//   },
-// });
+export default modelDefinition;
