6dc0001235c7b6f4bf8231c534cd3c90efa67151
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-07 07:59:57 -0500

    Convert medium model to use shared esrgan (#836)
    * Convert medium model to use shared esrgan
    
    * Update package json

---
+++ models/esrgan-medium/src/getModelDefinition.ts
@@ -0,0 +1,26 @@
+import { ModelDefinitionFn, } from '@upscalerjs/core';
+import { Scale, getESRGANModelDefinition, } from '../../../packages/shared/src/esrgan';
+import { NAME, VERSION, } from './constants.generated';
+
+const getModelDefinition = (scale: Scale, modelFileName: string): ModelDefinitionFn => getESRGANModelDefinition({
+  scale,
+  path: `models/${scale}x/model.json`,
+  name: NAME,
+  version: VERSION,
+  meta: {
+    C: 1,
+    D: 10,
+    G: 64,
+    G0: 64,
+    T: 10,
+    architecture: "rdn",
+    patchSize: scale === 3 ? 129 : 128,
+    size: 'slim',
+    artifactReducing: false,
+    sharpening: false,
+    dataset: 'div2k',
+    modelFileName,
+  },
+});
+
+export default getModelDefinition;
