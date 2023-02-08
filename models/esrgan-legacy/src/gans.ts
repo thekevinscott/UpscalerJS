d5952e29a270a266307964b87399b8257f0fac08
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-08 07:09:52 -0500

    Revert GANs

--- models/esrgan-legacy/src/gans.ts
+++ models/esrgan-legacy/src/gans.ts
@@ -1,24 +1,77 @@
-import { ModelDefinitionFn, } from '@upscalerjs/core';
-import { getESRGANModelDefinition, } from '../../../packages/shared/src/esrgan';
-// import getModelDefinition from './getModelDefinition';
+import type { Tensor4D, } from '@tensorflow/tfjs-core';
+import type { ModelDefinition, ModelDefinitionFn, TF, } from '@upscalerjs/core';
 import { NAME, VERSION, } from './constants.generated';
 
-const SCALE = 2;
+const modelDefinition: ModelDefinitionFn = (tf: TF) => {
+  const Layer = tf.layers.Layer;
+  const SCALE = 4;
+  const BETA = 0.2;
 
-// const modelDefinition: ModelDefinitionFn = getModelDefinition(SCALE, 'gans', 'rrdn');
+  type Inputs = Tensor4D | Tensor4D[];
 
-const modelDefinition: ModelDefinitionFn = getESRGANModelDefinition({
-  scale: SCALE,
-  path: `models/gans/model.json`,
-  name: NAME,
-  version: VERSION,
-  meta: {
-    dataset: 'div2k',
-    architecture: 'rrdn',
-  },
-});
+  const isTensorArray = (inputs: Inputs): inputs is Tensor4D[] => {
+    return Array.isArray(inputs);
+  };
 
-// export default getModelDefinition;
+  const getInput = (inputs: Inputs): Tensor4D => {
+    if (isTensorArray(inputs)) {
+      return inputs[0];
+    }
+    return inputs;
+  };
+  class MultiplyBeta extends Layer {
+    beta: number;
 
+    constructor() {
+      super({});
+      this.beta = BETA;
+    }
+
+    call(inputs: Inputs) {
+      return tf.mul(getInput(inputs), this.beta);
+    }
+
+    static className = 'MultiplyBeta';
+  }
+
+  class PixelShuffle extends Layer {
+    scale: number;
+
+    constructor() {
+      super({});
+      this.scale = SCALE;
+    }
+
+    computeOutputShape(inputShape: number[]) {
+      return [inputShape[0], inputShape[1], inputShape[2], 3,];
+    }
+
+    call(inputs: Inputs) {
+      return tf.depthToSpace(getInput(inputs), this.scale, 'NHWC');
+    }
+
+    static className = 'PixelShuffle';
+  }
+
+  [MultiplyBeta, PixelShuffle,].forEach((layer) => {
+    tf.serialization.registerClass(layer);
+  });
+
+  const modelDefinition: ModelDefinition = {
+    scale: SCALE,
+    path: 'models/gans/model.json',
+    packageInformation: {
+      name: NAME,
+      version: VERSION,
+    },
+    meta: {
+      dataset: 'div2k',
+    },
+    inputRange: [0, 1,],
+    outputRange: [0, 1,],
+  };
+
+  return modelDefinition;
+};
 
 export default modelDefinition;
