0ffcd778fc6771cc3240ab416213c88c99b544d5
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-08 06:56:24 -0500

    Update gans to reference shared file directly

--- models/esrgan-legacy/src/gans.ts
+++ models/esrgan-legacy/src/gans.ts
@@ -1,8 +1,24 @@
 import { ModelDefinitionFn, } from '@upscalerjs/core';
-import getModelDefinition from './getModelDefinition';
+import { getESRGANModelDefinition, } from '../../../packages/shared/src/esrgan';
+// import getModelDefinition from './getModelDefinition';
+import { NAME, VERSION, } from './constants.generated';
 
 const SCALE = 2;
 
-const modelDefinition: ModelDefinitionFn = getModelDefinition(SCALE, 'gans', 'rrdn');
+// const modelDefinition: ModelDefinitionFn = getModelDefinition(SCALE, 'gans', 'rrdn');
+
+const modelDefinition: ModelDefinitionFn = getESRGANModelDefinition({
+  scale: SCALE,
+  path: `models/gans/model.json`,
+  name: NAME,
+  version: VERSION,
+  meta: {
+    dataset: 'div2k',
+    architecture: 'rrdn',
+  },
+});
+
+// export default getModelDefinition;
+
 
 export default modelDefinition;
