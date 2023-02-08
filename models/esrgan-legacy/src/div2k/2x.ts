91907559e18db7a369cf239fd9cc6df2992ec6a8
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-08 07:11:18 -0500

    Try to update 2x model

--- models/esrgan-legacy/src/div2k/2x.ts
+++ models/esrgan-legacy/src/div2k/2x.ts
@@ -1,8 +1,17 @@
-import { ModelDefinition, } from '@upscalerjs/core';
-import _getModelDefinitionDEPRECATED from '../utils/getModelDefinitionDeprecated';
+// import { ModelDefinition, } from '@upscalerjs/core';
+// import _getModelDefinitionDEPRECATED from '../utils/getModelDefinitionDeprecated';
+
+// const SCALE = 2;
+
+// const modelDefinition: ModelDefinition = _getModelDefinitionDEPRECATED(SCALE, `div2k/${SCALE}x`);
+
+// export default modelDefinition;
+
+import { ModelDefinitionFn, } from '@upscalerjs/core';
+import getModelDefinition from '../getModelDefinition';
 
 const SCALE = 2;
 
-const modelDefinition: ModelDefinition = _getModelDefinitionDEPRECATED(SCALE, `div2k/${SCALE}x`);
+const modelDefinition: ModelDefinitionFn = getModelDefinition(SCALE, `div2k/${SCALE}x`);
 
 export default modelDefinition;
