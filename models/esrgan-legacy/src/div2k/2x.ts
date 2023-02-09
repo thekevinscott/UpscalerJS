ca133dfa780c98f3549c8779fe93f09555393d28
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-09 06:04:48 -0500

    Update other div2k models to leverage shared

--- models/esrgan-legacy/src/div2k/2x.ts
+++ models/esrgan-legacy/src/div2k/2x.ts
@@ -1,12 +1,3 @@
-// import { ModelDefinition, } from '@upscalerjs/core';
-// import _getModelDefinitionDEPRECATED from '../utils/getModelDefinitionDeprecated';
-
-// const SCALE = 2;
-
-// const modelDefinition: ModelDefinition = _getModelDefinitionDEPRECATED(SCALE, `div2k/${SCALE}x`);
-
-// export default modelDefinition;
-
 import { ModelDefinitionFn, } from '@upscalerjs/core';
 import getModelDefinition from '../getModelDefinition';
 
--- models/esrgan-legacy/src/div2k/4x.ts
+++ models/esrgan-legacy/src/div2k/4x.ts
@@ -1,8 +1,8 @@
-import { ModelDefinition, } from '@upscalerjs/core';
-import _getModelDefinitionDEPRECATED from '../utils/getModelDefinitionDeprecated';
+import { ModelDefinitionFn, } from '@upscalerjs/core';
+import getModelDefinition from '../getModelDefinition';
 
 const SCALE = 4;
 
-const modelDefinition: ModelDefinition = _getModelDefinitionDEPRECATED(SCALE, `div2k/${SCALE}x`);
+const modelDefinition: ModelDefinitionFn = getModelDefinition(SCALE, `div2k/${SCALE}x`);
 
 export default modelDefinition;
