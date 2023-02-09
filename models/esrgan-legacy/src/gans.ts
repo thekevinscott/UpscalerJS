640e9305d80278b65526acb41c9dd30211b74ab0
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-09 07:01:04 -0500

    Add a comment and clean up types

--- models/esrgan-legacy/src/gans.ts
+++ models/esrgan-legacy/src/gans.ts
@@ -3,6 +3,10 @@ import type { ModelDefinition, ModelDefinitionFn, TF, } from '@upscalerjs/core';
 import { NAME, VERSION, } from './constants.generated';
 
 const modelDefinition: ModelDefinitionFn = (tf: TF) => {
+  /** For some reason, the below custom registered layers
+   * are required even though we're redefining them in the shared esrgan
+   * package. I'm not sure why this is the case, but it's a workaround.
+   */
   const Layer = tf.layers.Layer;
   const SCALE = 4;
   const BETA = 0.2;
--- models/esrgan-thick/src/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Tensor4D, } from '@tensorflow/tfjs-core';
-
-export type Scale = 2 | 3 | 4 | 8;
-
-export type Inputs = Tensor4D | Tensor4D[];
