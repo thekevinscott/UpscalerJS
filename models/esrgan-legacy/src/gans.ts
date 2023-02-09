896a13cd07aeac7d5e5515dc76b82c2ccd87c192
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-09 06:37:01 -0500

    GANs should leverage the shared esrgan file

--- models/esrgan-legacy/src/gans.ts
+++ models/esrgan-legacy/src/gans.ts
@@ -1,63 +1,9 @@
-import type { Tensor4D, } from '@tensorflow/tfjs-core';
 import type { ModelDefinitionFn, TF, } from '@upscalerjs/core';
 import { getESRGANModelDefinition, } from '../../../packages/shared/src/esrgan';
 import { NAME, VERSION, } from './constants.generated';
 
+const SCALE = 4;
 const modelDefinition: ModelDefinitionFn = (tf: TF) => {
-  const Layer = tf.layers.Layer;
-  const SCALE = 4;
-  const BETA = 0.2;
-
-  type Inputs = Tensor4D | Tensor4D[];
-
-  const isTensorArray = (inputs: Inputs): inputs is Tensor4D[] => {
-    return Array.isArray(inputs);
-  };
-
-  const getInput = (inputs: Inputs): Tensor4D => {
-    if (isTensorArray(inputs)) {
-      return inputs[0];
-    }
-    return inputs;
-  };
-  class MultiplyBeta extends Layer {
-    beta: number;
-
-    constructor() {
-      super({});
-      this.beta = BETA;
-    }
-
-    call(inputs: Inputs) {
-      return tf.mul(getInput(inputs), this.beta);
-    }
-
-    static className = 'MultiplyBeta';
-  }
-
-  class PixelShuffle extends Layer {
-    scale: number;
-
-    constructor() {
-      super({});
-      this.scale = SCALE;
-    }
-
-    computeOutputShape(inputShape: number[]) {
-      return [inputShape[0], inputShape[1], inputShape[2], 3,];
-    }
-
-    call(inputs: Inputs) {
-      return tf.depthToSpace(getInput(inputs), this.scale, 'NHWC');
-    }
-
-    static className = 'PixelShuffle';
-  }
-
-  [MultiplyBeta, PixelShuffle,].forEach((layer) => {
-    tf.serialization.registerClass(layer);
-  });
-
   return getESRGANModelDefinition({
     scale: SCALE, 
     path: `models/gans/model.json`,
