9da97c0adebed85240ea42d844e513fcb22e3580
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-09 06:22:38 -0500

    Update GANs model

--- models/esrgan-thick/src/getModelDefinition.ts
+++ models/esrgan-thick/src/getModelDefinition.ts
@@ -15,7 +15,7 @@ const getModelDefinition = (scale: Scale, modelFileName: string): ModelDefinitio
     T: 10,
     architecture: "rrdn",
     patchSize: scale === 3 ? 129 : 128,
-    size: 'slim',
+    size: 'thick',
     artifactReducing: false,
     sharpening: false,
     dataset: 'div2k',
