0ee76f1387d31e522f711cef0178f0c933760c62
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-02-11 16:41:38 -0500

    Remove unused imports

--- test/integration/browserstack.dependencies.ts
+++ test/integration/browserstack.dependencies.ts
@@ -1,6 +1,4 @@
 import { bundleEsbuild } from "../lib/esm-esbuild/prepare";
-import { bundleWebpack, prepareScriptBundleForESM } from "../lib/esm-webpack/prepare";
-import { prepareScriptBundleForUMD } from "../lib/umd/prepare";
 
 const dependencies = {
   browser: [
