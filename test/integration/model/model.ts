718c7aa7f281847f4e40b86f02166cd937d86671
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-03-02 13:25:48 -0500

    Leverage jest .each format (#912)

--- test/integration/model/model.ts
+++ test/integration/model/model.ts
@@ -25,111 +25,113 @@ const JEST_TIMEOUT = 60 * 1000 * 5;
 jest.setTimeout(JEST_TIMEOUT); // 5 minute timeout
 jest.retryTimes(0);
 
+const getModels = (platform: 'browser' | 'node') => getFilteredModels({
+  specificPackage: undefined,
+  specificModel: undefined,
+  filter: (packageName, model) => {
+    const packagePath = path.resolve(MODELS_DIR, packageName);
+    const packageJSON = getPackageJSON(packagePath);
+    const supportedPlatforms = packageJSON['@upscalerjs']?.models?.[model.export]?.supportedPlatforms;
 
-if (PLATFORMS?.includes('browser')) {
-  const filteredPackagesAndModels = getFilteredModels({
-    specificPackage: undefined,
-    specificModel: undefined,
-    filter: (packageName, model) => {
-      const packagePath = path.resolve(MODELS_DIR, packageName);
-      const packageJSON = getPackageJSON(packagePath);
-      const supportedPlatforms = packageJSON['@upscalerjs']?.models?.[model.export]?.supportedPlatforms;
+    return supportedPlatforms === undefined || supportedPlatforms.includes(platform);
+  },
+});
 
-      return supportedPlatforms === undefined || supportedPlatforms.includes('browser');
-    },
+const filterModels = (models: AvailableModel[], ...keys: (keyof AvailableModel)[]) => models.map(model => {
+  return keys.map(key => {
+    const value = model[key];
+    if (typeof value !== 'string') {
+      throw new Error(`Expected ${key} to be a string, but got ${value} (${typeof value})`);
+    }
+    return value || 'index';
   });
+});
 
-  describe('Browser ESM Model Tests', () => {
-    const testRunner = new BrowserTestRunner({
-      name: 'esm',
-      mockCDN: esbuildMockCDN,
-      dist: ESBUILD_DIST,
-      trackTime: TRACK_TIME,
-      log: LOG,
-      verbose: VERBOSE,
-      usePNPM: USE_PNPM,
-    });
-    const page = () => testRunner.page;
 
-    beforeAll(async function beforeAll() {
-      await testRunner.beforeAll();
-    }, 60000);
+describe('Model Tests', () => {
+  describe.each(PLATFORMS?.includes('browser') ? [[]] : [])('Browser', () => {
+    const filteredPackagesAndModels = getModels('browser');
 
-    afterAll(async function modelAfterAll() {
-      await testRunner.afterAll();
-    }, 10000);
+    describe('ESM', () => {
+      const testRunner = new BrowserTestRunner({
+        name: 'esm',
+        mockCDN: esbuildMockCDN,
+        dist: ESBUILD_DIST,
+        trackTime: TRACK_TIME,
+        log: LOG,
+        verbose: VERBOSE,
+        usePNPM: USE_PNPM,
+      });
+      const page = () => testRunner.page;
 
-    beforeEach(async function beforeEach() {
-      await testRunner.beforeEach('| Loaded');
-    });
+      beforeAll(async function beforeAll() {
+        await testRunner.beforeAll();
+      }, 60000);
 
-    afterEach(async function afterEach() {
-      await testRunner.afterEach();
-    });
+      afterAll(async function modelAfterAll() {
+        await testRunner.afterAll();
+      }, 10000);
 
-    filteredPackagesAndModels.forEach(([packageName, filteredModels]) => {
-      describe(packageName, () => {
-        filteredModels.forEach(({ esm,  }) => {
-          const esmName = esm || 'index';
-          it(esmName, async () => {
-            await testRunner.navigateToServer('| Loaded');
-            const result = await page().evaluate(([packageName, modelName]) => {
-              if (!modelName) {
-                throw new Error(`No model name found for package ${packageName}`);
-              }
-              // TODO: window fails to be typed correctly in CI
-              // https://github.com/thekevinscott/UpscalerJS/runs/7176553596?check_suite_focus=true#step:7:60
-              // Locally it works fine
-              const modelDefinition = (window as any)[packageName][modelName];
-              if (!modelDefinition) {
-                throw new Error(`No model definition found for package name ${packageName} and model name ${modelName}`);
-              }
-              const upscaler = new window['Upscaler']({
-                model: modelDefinition,
-              });
-              return upscaler.execute(window['fixtures'][packageName]);
-            }, [packageName, esmName]);
-            const resultPath = path.resolve(MODELS_DIR, packageName, "test/__fixtures__", esmName, "result.png")
-            const outputsPath = path.resolve(TMP_DIR, 'test-output/diff/browser/umd', packageName, esmName);
-            const diffPath = path.resolve(outputsPath, `diff.png`);
-            const upscaledPath = path.resolve(outputsPath, `upscaled.png`);
-            checkImage(result, resultPath, diffPath, upscaledPath);
-          });
+      beforeEach(async function beforeEach() {
+        await testRunner.beforeEach('| Loaded');
+      });
+
+      afterEach(async function afterEach() {
+        await testRunner.afterEach();
+      });
+
+      describe.each(filteredPackagesAndModels)('%s', (packageName, filteredModels) => {
+        test.each(filterModels(filteredModels, 'esm'))('%s', async (modelName) => {
+          await testRunner.navigateToServer('| Loaded');
+          const result = await page().evaluate(([packageName, modelName]) => {
+            // TODO: window fails to be typed correctly in CI
+            // https://github.com/thekevinscott/UpscalerJS/runs/7176553596?check_suite_focus=true#step:7:60
+            // Locally it works fine
+            const modelDefinition = (window as any)[packageName][modelName];
+            if (!modelDefinition) {
+              throw new Error(`No model definition found for package name ${packageName} and model name ${modelName}`);
+            }
+            const upscaler = new window['Upscaler']({
+              model: modelDefinition,
+            });
+            return upscaler.execute(window['fixtures'][packageName]);
+          }, [packageName, modelName]);
+          const resultPath = path.resolve(MODELS_DIR, packageName, "test/__fixtures__", modelName, "result.png")
+          const outputsPath = path.resolve(TMP_DIR, 'test-output/diff/browser/umd', packageName, modelName);
+          const diffPath = path.resolve(outputsPath, `diff.png`);
+          const upscaledPath = path.resolve(outputsPath, `upscaled.png`);
+          checkImage(result, resultPath, diffPath, upscaledPath);
         });
       });
     });
-  });
 
-  describe('Browser UMD Model Tests', () => {
-    const UMD_PORT = 8096;
-    const umdTestRunner = new BrowserTestRunner({
-      name: 'umd',
-      mockCDN: umdMockCDN,
-      dist: UMD_DIST,
-      port: UMD_PORT,
-    });
+    describe('UMD', () => {
+      const UMD_PORT = 8096;
+      const umdTestRunner = new BrowserTestRunner({
+        name: 'umd',
+        mockCDN: umdMockCDN,
+        dist: UMD_DIST,
+        port: UMD_PORT,
+      });
 
-    beforeAll(async function modelBeforeAll() {
-      await umdTestRunner.beforeAll();
-    }, 20000);
+      beforeAll(async function modelBeforeAll() {
+        await umdTestRunner.beforeAll();
+      }, 20000);
 
-    afterAll(async function modelAfterAll() {
-      await umdTestRunner.afterAll();
-    }, 30000);
+      afterAll(async function modelAfterAll() {
+        await umdTestRunner.afterAll();
+      }, 30000);
 
-    beforeEach(async function beforeEach() {
-      await umdTestRunner.beforeEach(null);
-    });
+      beforeEach(async function beforeEach() {
+        await umdTestRunner.beforeEach(null);
+      });
 
-    afterEach(async function afterEach() {
-      await umdTestRunner.afterEach();
-    });
+      afterEach(async function afterEach() {
+        await umdTestRunner.afterEach();
+      });
 
-    filteredPackagesAndModels.forEach(([packageName, filteredModels]) => {
-      describe(packageName, () => {
-        filteredModels.forEach(({ esm, umd: umdName }) => {
-          const esmName = esm || 'index';
-          it(esmName, async () => {
+      describe.each(filteredPackagesAndModels)('%s', (packageName, filteredModels) => {
+        test.each(filterModels(filteredModels, 'esm', 'umd'))('%s', async (esmName, umdName) => {
           const result = await umdTestRunner.page.evaluate(([umdName, packageName]) => {
             const model: ModelDefinition = (<any>window)[umdName];
             const upscaler = new window['Upscaler']({
@@ -142,98 +144,84 @@ if (PLATFORMS?.includes('browser')) {
           const diffPath = path.resolve(outputsPath, `diff.png`);
           const upscaledPath = path.resolve(outputsPath, `upscaled.png`);
           checkImage(result, resultPath, diffPath, upscaledPath);
-          });
         });
       });
     });
   });
-}
 
-if (PLATFORMS?.includes('node')) {
-  const main: Main = async (deps) => {
-    const {
-      Upscaler,
-      tf,
-      base64ArrayBuffer,
-      imagePath,
-      model,
-      fs,
-    } = deps;
-    console.log('Running main script with model', JSON.stringify(typeof model === 'function' ? model(tf) : model, null, 2));
+  describe.each(PLATFORMS?.includes('node') ? [[]] : [])('Node', () => {
+    describe('CJS', () => {
+      const main: Main = async (deps) => {
+        const {
+          Upscaler,
+          tf,
+          base64ArrayBuffer,
+          imagePath,
+          model,
+          fs,
+        } = deps;
+        console.log('Running main script with model', JSON.stringify(typeof model === 'function' ? model(tf) : model, null, 2));
 
-    const upscaler = new Upscaler({
-      model,
-    });
+        const upscaler = new Upscaler({
+          model,
+        });
 
-    const imageData = fs.readFileSync(imagePath);
-    const tensor = tf.node.decodeImage(imageData).slice([0, 0, 0], [-1, -1, 3]); // discard alpha channel, if exists
-    const result = await upscaler.execute(tensor, {
-      output: 'tensor',
-      patchSize: 64,
-      padding: 6,
-      progress: console.log,
-    });
-    tensor.dispose();
-    // because we are requesting a tensor, it is possible that the tensor will
-    // contain out-of-bounds pixels; part of the value of this test is ensuring
-    // that those values are clipped in a post-process step.
-    const upscaledImage = await tf.node.encodePng(result);
-    result.dispose();
-    return base64ArrayBuffer(upscaledImage);
-  };
+        const imageData = fs.readFileSync(imagePath);
+        const tensor = tf.node.decodeImage(imageData).slice([0, 0, 0], [-1, -1, 3]); // discard alpha channel, if exists
+        const result = await upscaler.execute(tensor, {
+          output: 'tensor',
+          patchSize: 64,
+          padding: 6,
+          progress: console.log,
+        });
+        tensor.dispose();
+        // because we are requesting a tensor, it is possible that the tensor will
+        // contain out-of-bounds pixels; part of the value of this test is ensuring
+        // that those values are clipped in a post-process step.
+        const upscaledImage = await tf.node.encodePng(result);
+        result.dispose();
+        return base64ArrayBuffer(upscaledImage);
+      };
+      const testRunner = new NodeTestRunner({
+        main,
+        trackTime: false,
+        dependencies: {
+          'tf': `@tensorflow/tfjs-node${USE_GPU ? '-gpu' : ''}`,
+          'Upscaler': `${LOCAL_UPSCALER_NAME}/node`,
+          'fs': 'fs',
+          'base64ArrayBuffer': path.resolve(__dirname, '../../lib/utils/base64ArrayBuffer'),
+        },
+      });
 
-  describe('Node CJS Model Tests', () => {
-    const testRunner = new NodeTestRunner({
-      main,
-      trackTime: false,
-      dependencies: {
-        'tf': `@tensorflow/tfjs-node${USE_GPU ? '-gpu' : ''}`,
-        'Upscaler': `${LOCAL_UPSCALER_NAME}/node`,
-        'fs': 'fs',
-        'base64ArrayBuffer': path.resolve(__dirname, '../../lib/utils/base64ArrayBuffer'),
-      },
-    });
+      const filteredPackagesAndModels = getModels('node');
 
-    const filteredPackagesAndModels = getFilteredModels({
-      filter: (packageName, model) => {
-        const packagePath = path.resolve(MODELS_DIR, packageName);
-        const packageJSON = getPackageJSON(packagePath);
-        const supportedPlatforms = packageJSON['@upscalerjs']?.models?.[model.export]?.supportedPlatforms;
-
-        return supportedPlatforms === undefined || supportedPlatforms.includes('node');
-      },
-    });
-    filteredPackagesAndModels.forEach(([packageName, filteredModels]) => {
-      describe(packageName, () => {
-        filteredModels.forEach(({ cjs }) => {
-          const cjsName = cjs || 'index';
-          it(cjsName, async () => {
-            const importPath = path.join(LOCAL_UPSCALER_NAMESPACE, packageName, cjsName === 'index' ? '' : `/${cjsName}`);
-            const modelPackageDir = path.resolve(MODELS_DIR, packageName, 'test/__fixtures__');
-            const fixturePath = path.resolve(modelPackageDir, 'fixture.png');
-            const result = await testRunner.run({
-              dependencies: {
-                customModel: importPath,
-              },
-              globals: {
-                model: 'customModel',
-                imagePath: JSON.stringify(fixturePath),
-              }
-            });
-
-            expect(result).not.toEqual('');
-            const formattedResult = `data:image/png;base64,${result}`;
-            const resultPath = path.resolve(MODELS_DIR, packageName, "test/__fixtures__", cjsName, "result.png")
-            const outputsPath = path.resolve(TMP_DIR, 'test-output/diff/node', packageName, cjsName);
-            const diffPath = path.resolve(outputsPath, `diff.png`);
-            const upscaledPath = path.resolve(outputsPath, `upscaled.png`);
-            checkImage(formattedResult, resultPath, diffPath, upscaledPath);
+      describe.each(filteredPackagesAndModels)('%s', (packageName, filteredModels) => {
+        test.each(filterModels(filteredModels, 'cjs'))('%s', async (cjsName) => {
+          const importPath = path.join(LOCAL_UPSCALER_NAMESPACE, packageName, cjsName === 'index' ? '' : `/${cjsName}`);
+          const modelPackageDir = path.resolve(MODELS_DIR, packageName, 'test/__fixtures__');
+          const fixturePath = path.resolve(modelPackageDir, 'fixture.png');
+          const result = await testRunner.run({
+            dependencies: {
+              customModel: importPath,
+            },
+            globals: {
+              model: 'customModel',
+              imagePath: JSON.stringify(fixturePath),
+            }
           });
+
+          expect(result).not.toEqual('');
+          const formattedResult = `data:image/png;base64,${result}`;
+          const resultPath = path.resolve(MODELS_DIR, packageName, "test/__fixtures__", cjsName, "result.png")
+          const outputsPath = path.resolve(TMP_DIR, 'test-output/diff/node', packageName, cjsName);
+          const diffPath = path.resolve(outputsPath, `diff.png`);
+          const upscaledPath = path.resolve(outputsPath, `upscaled.png`);
+          checkImage(formattedResult, resultPath, diffPath, upscaledPath);
         });
       });
     });
   });
-}
+});
 
 declare global {
   interface Window {
