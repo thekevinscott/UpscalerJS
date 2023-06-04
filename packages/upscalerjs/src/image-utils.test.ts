f74ec0e63bf6e6e9b4ab0c4f7115a49c8ed05131
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-06-04 16:18:22 -0400

    Add function for getting patch size with subtractive padding

---
+++ packages/upscalerjs/src/image-utils.test.ts
@@ -0,0 +1,792 @@
+import { getPatchesFromImage } from "./image-utils";
+import { Coordinate, Patch } from "./types";
+
+interface PartialPatch {
+  pre?: Partial<Patch['pre']>;
+  post?: Partial<Patch['post']>;
+}
+
+describe('getPatchesFromImage', () => {
+  type TestCase = [
+    string, // test name
+    Coordinate, // image size
+    number, // patch size
+    number, // padding
+    Patch[], // expectation
+  ];
+
+  const buildTestCaseExpectation = (patches: PartialPatch[], { pre: globalPre = {} , post: globalPost = {} }: PartialPatch = {}): Patch[] => patches.map(({ pre = {}, post = {} }) => ({
+    pre: {
+      ...globalPre,
+      ...pre,
+    },
+    post: {
+      ...globalPost,
+      ...post,
+    },
+  } as Patch));
+
+  const testCases: TestCase[] = [
+    [
+      'a patch size matching image size',
+      [2,2],
+      2,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        },
+      }]),
+    ],
+
+    [
+      'a patch size larger than image size',
+      [2,2],
+      4,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        },
+      }]),
+    ],
+
+    [
+      'a patch size taller than image size in one dimension',
+      [6,2],
+      4,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+          size: [2, 4,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [2, 4,],
+        },
+      }, {
+        pre: {
+          origin: [0, 2,],
+          size: [2, 4,],
+        },
+        post: {
+          origin: [0, 2,],
+          size: [2, 2,],
+        },
+      }]),
+    ],
+
+    [
+      'a smaller patch size that fits evenly into a taller image',
+      [2,4],
+      2,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        },
+      }, {
+        pre: {
+          origin: [2, 0,],
+          size: [2, 2,],
+        },
+      }], {
+        post: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        }
+      }),
+    ],
+
+    [
+      'a smaller patch size that fits evenly into a wider image',
+      [4,2],
+      2,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        },
+      }, {
+        pre: {
+          origin: [0, 2,],
+          size: [2, 2,],
+        },
+      }], {
+        post: {
+          origin: [0, 0,],
+          size: [2, 2,],
+        }
+      }),
+    ],
+
+    [
+      'an odd patch size that does not fit into image',
+      [4,4],
+      3,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [3, 3,],
+        },
+      }, {
+        pre: {
+          origin: [0, 1,],
+        },
+        post: {
+          origin: [0, 2,],
+          size: [3, 1,],
+        },
+      }, {
+        pre: {
+          origin: [1, 0,],
+        },
+        post: {
+          origin: [2, 0,],
+          size: [1, 3,],
+        },
+      }, {
+        pre: {
+          origin: [1, 1],
+        },
+        post: {
+          origin: [2, 2,],
+          size: [1, 1,],
+        },
+      }], {
+        pre: {
+          size: [3,3],
+        },
+      }),
+    ],
+
+    [
+      'an odd patch size that does not fit into a taller image',
+      [4,5],
+      3,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [3, 3,],
+        },
+      }, {
+        pre: {
+          origin: [0, 1,],
+        },
+        post: {
+          origin: [0, 2,],
+          size: [3, 1,],
+        },
+      }, {
+        pre: {
+          origin: [2, 0,],
+        },
+        post: {
+          origin: [1, 0,],
+          size: [2, 3,],
+        },
+      }, {
+        pre: {
+          origin: [2, 1],
+        },
+        post: {
+          origin: [1, 2,],
+          size: [2, 1,],
+        },
+      }], {
+        pre: {
+          size: [3,3],
+        },
+      }),
+    ],
+    [
+      'an odd patch size that does not fit into a wider image',
+      [5, 4],
+      3,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [3, 3,],
+        },
+      }, {
+        pre: {
+          origin: [0, 2,],
+        },
+        post: {
+          origin: [0, 1,],
+          size: [3, 2,],
+        },
+      }, {
+        pre: {
+          origin: [1, 0,],
+        },
+        post: {
+          origin: [2, 0,],
+          size: [1, 3,],
+        },
+      }, {
+        pre: {
+          origin: [1, 2],
+        },
+        post: {
+          origin: [2, 1,],
+          size: [1, 2,],
+        },
+      }], {
+        pre: {
+          size: [3, 3],
+        },
+      }),
+    ],
+
+    [
+      'an even patch size that does not fit into image',
+      [5,5],
+      4,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [4, 4,],
+        },
+      }, {
+        pre: {
+          origin: [0, 1,],
+        },
+        post: {
+          origin: [0, 3,],
+          size: [4, 1,],
+        },
+      }, {
+        pre: {
+          origin: [1, 0,],
+        },
+        post: {
+          origin: [3, 0,],
+          size: [1, 4,],
+        },
+      }, {
+        pre: {
+          origin: [1, 1],
+        },
+        post: {
+          origin: [3, 3,],
+          size: [1, 1,],
+        },
+      }], {
+        pre: {
+          size: [4,4],
+        },
+      }),
+    ],
+
+    [
+      'an even patch size that does not fit into a taller image',
+      [5,6],
+      4,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [4, 4,],
+        },
+      }, {
+        pre: {
+          origin: [0, 1,],
+        },
+        post: {
+          origin: [0, 3,],
+          size: [4, 1,],
+        },
+      }, {
+        pre: {
+          origin: [2, 0,],
+        },
+        post: {
+          origin: [2, 0,],
+          size: [2, 4,],
+        },
+      }, {
+        pre: {
+          origin: [2, 1],
+        },
+        post: {
+          origin: [2, 3,],
+          size: [2, 1,],
+        },
+      }], {
+        pre: {
+          size: [4,4],
+        },
+      }),
+    ],
+
+    [
+      'an even patch size that does not fit into a wider image',
+      [6,5],
+      4,
+      0,
+      buildTestCaseExpectation([{
+        pre: {
+          origin: [0, 0,],
+        },
+        post: {
+          origin: [0, 0,],
+          size: [4, 4,],
+        },
+      }, {
+        pre: {
+          origin: [0, 2],
+        },
+        post: {
+          origin: [0, 2,],
+          size: [4, 2,],
+        },
+      }, {
+        pre: {
+          origin: [1, 0,],
+        },
+        post: {
+          origin: [3, 0,],
+          size: [1, 4,],
+        },
+      }, {
+        pre: {
+          origin: [1, 2],
+        },
+        post: {
+          origin: [3, 2,],
+          size: [1, 2,],
+        },
+      }], {
+        pre: {
+          size: [4,4],
+        },
+      }),
+    ],
+
+    [
+      'an odd patch size with odd padding',
+      [5, 5],
+      3,
+      1,
+      buildTestCaseExpectation([
+        // 0
+        {
+          pre: { origin: [0, 0,], },
+          post: {
+            origin: [0, 0,],
+            size: [2, 2,],
+          },
+        },
+        // 1
+        {
+          pre: { origin: [0, 1,], },
+          post: {
+            origin: [0, 1,],
+            size: [2, 1,],
+          },
+        },
+        // 2
+        {
+          pre: { origin: [0, 2,], },
+          post: {
+            origin: [0, 1,],
+            size: [2, 2,],
+          },
+        },
+        // 3
+        {
+          pre: { origin: [1, 0,], },
+          post: {
+            origin: [1, 0,],
+            size: [1, 2,],
+          },
+        },
+        // 4
+        {
+          pre: { origin: [1, 1,], },
+          post: {
+            origin: [1, 1,],
+            size: [1, 1,],
+          },
+        },
+        // 5
+        {
+          pre: { origin: [1, 2,], },
+          post: {
+            origin: [1, 1,],
+            size: [1, 2,],
+          },
+        },
+        // 6
+        {
+          pre: { origin: [2, 0,], },
+          post: {
+            origin: [1, 0,],
+            size: [2, 2,],
+          },
+        },
+        // 7
+        {
+          pre: { origin: [2, 1,], },
+          post: {
+            origin: [1, 1,],
+            size: [2, 1,],
+          },
+        },
+        // 8
+        {
+          pre: { origin: [2, 2,], },
+          post: {
+            origin: [1, 1,],
+            size: [2, 2,],
+          },
+        }], {
+        pre: {
+          size: [3, 3],
+        }
+      }),
+    ],
+
+    [
+      'an odd patch size with even padding',
+      [6, 6],
+      5,
+      2,
+      buildTestCaseExpectation([
+        // 0, we have 3x3 pixels covered
+        {
+          pre: { origin: [0, 0,], },
+          post: {
+            origin: [0, 0,],
+            size: [3, 3],
+          },
+        },
+        // 1, now we have covered the 4th pixel
+        {
+          pre: { origin: [0, 1,], },
+          post: {
+            origin: [0, 2,],
+            size: [3, 3,],
+          },
+        },
+        // 2
+        {
+          pre: { origin: [1, 0,], },
+          post: {
+            origin: [2, 0,],
+            size: [3, 3,],
+          },
+        },
+        // 3
+        {
+          pre: { origin: [1, 1,], },
+          post: {
+            origin: [2, 2,],
+            size: [3, 3,],
+          },
+        }], {
+        pre: {
+          size: [5, 5],
+        }
+      }),
+    ],
+
+    [
+      'an even patch size with odd padding',
+      [6, 6],
+      4,
+      1,
+      buildTestCaseExpectation([
+        // 0
+        {
+          pre: { origin: [0, 0,], },
+          post: {
+            origin: [0, 0,],
+            size: [3, 3],
+          },
+        },
+        // 1
+        {
+          pre: { origin: [0, 2,], },
+          post: {
+            origin: [0, 1,],
+            size: [3, 3,],
+          },
+        },
+        // 2
+        {
+          pre: { origin: [2, 0], },
+          post: {
+            origin: [1, 0],
+            size: [3, 3],
+          },
+        },
+        // 3
+        {
+          pre: { origin: [2, 2], },
+          post: {
+            origin: [1, 1],
+            size: [3, 3],
+          },
+        }], {
+        pre: {
+          size: [4, 4],
+        }
+      }),
+    ],
+
+    [
+      'an even patch size with even padding',
+      [8, 8],
+      6,
+      2,
+      buildTestCaseExpectation([
+        // 0
+        {
+          pre: { origin: [0, 0,], },
+          post: {
+            origin: [0, 0,],
+            size: [4, 4],
+          },
+        },
+        // 1
+        {
+          pre: { origin: [0, 2,], },
+          post: {
+            origin: [0, 2,],
+            size: [4, 4,],
+          },
+        },
+        // 2
+        {
+          pre: { origin: [2, 0], },
+          post: {
+            origin: [2, 0],
+            size: [4, 4],
+          },
+        },
+        // 3
+        {
+          pre: { origin: [2, 2], },
+          post: {
+            origin: [2, 2],
+            size: [4, 4],
+          },
+        }], {
+        pre: {
+          size: [6, 6],
+        }
+      }),
+    ],
+
+    [
+      'a sanity check',
+      [9, 9],
+      5,
+      1,
+      buildTestCaseExpectation([
+        // 0
+        {
+          pre: { origin: [0, 0,], },
+          post: {
+            origin: [0, 0,],
+            size: [4, 4],
+          },
+        },
+        // 1
+        {
+          pre: { origin: [0, 3,], },
+          post: {
+            origin: [0, 1,],
+            size: [4, 3,],
+          },
+        },
+        // 2
+        {
+          pre: { origin: [0, 4], },
+          post: {
+            origin: [0, 3],
+            size: [4, 2],
+          },
+        }, 
+        // 3
+        {
+          pre: { origin: [3, 0,], },
+          post: {
+            origin: [1, 0,],
+            size: [3, 4],
+          },
+        },
+        // 4
+        {
+          pre: { origin: [3, 3,], },
+          post: {
+            origin: [1, 1,],
+            size: [3, 3,],
+          },
+        },
+        // 5
+        {
+          pre: { origin: [3, 4], },
+          post: {
+            origin: [1, 3],
+            size: [3, 2],
+          },
+        },
+        // 6
+        {
+          pre: { origin: [4, 0,], },
+          post: {
+            origin: [3, 0,],
+            size: [2, 4],
+          },
+        },
+        // 7
+        {
+          pre: { origin: [4, 3,], },
+          post: {
+            origin: [3, 1,],
+            size: [2, 3,],
+          },
+        },
+        // 8
+        {
+          pre: { origin: [4, 4], },
+          post: {
+            origin: [3, 3],
+            size: [2, 2],
+          },
+        }
+      ], {
+        pre: {
+          size: [5, 5],
+        }
+      }),
+    ],
+
+    [
+      'a larger sanity check',
+      [200, 300],
+      128,
+      2,
+      buildTestCaseExpectation([
+        // 0
+        {
+          pre: { origin: [0, 0,], },
+          post: {
+            origin: [0, 0,],
+            size: [126, 126],
+          },
+        },
+        // 1
+        {
+          pre: { origin: [0, 72,], },
+          post: {
+            origin: [0, 54,],
+            size: [126, 74],
+          },
+        },
+        // 2
+        {
+          pre: { origin: [124, 0], },
+          post: {
+            origin: [2, 0],
+            size: [124, 126],
+          },
+        }, 
+        // 3
+        {
+          pre: { origin: [124, 72], },
+          post: {
+            origin: [2, 54],
+            size: [124, 74],
+          },
+        },
+        // 4
+        {
+          pre: { origin: [172, 0,], },
+          post: {
+            origin: [78, 0,],
+            size: [50, 126,],
+          },
+        },
+        // 5
+        {
+          pre: { origin: [172, 72,], },
+          post: {
+            origin: [78, 54,],
+            size: [50, 74,],
+          },
+        },
+      ], {
+        pre: {
+          size: [128, 128],
+        }
+      }),
+    ],
+  ];
+  test.each(testCases)(
+    [
+      '%s',
+      'image size: %p',
+      'patch size: %i',
+      'padding: %i',
+    ].join(' | '),
+    (_, imageSize, patchSize, padding, _expectation) => {
+      const result = getPatchesFromImage(imageSize, patchSize, padding);
+      expect(result.length).toEqual(_expectation.length);
+      for (let i = 0; i < result.length; i++) {
+        const res = result[i];
+        const exp = _expectation[i];
+        const keys: ('pre' | 'post')[] = ['pre', 'post'];
+        for (const key of keys) {
+          try {
+            expect(res[key]).toEqual(exp[key]);
+          } catch (err) {
+            throw new Error([
+              `Fail for index: ${i} and key: ${key}`,
+              ...[
+                [
+                  ['Result', res[key]],
+                  ['Expectation', exp[key]],
+                ].map(([key, obj]) => `${key}[${i}]: ${JSON.stringify(obj, Object.keys(obj).sort(), 2)}`)
+              ],
+            ].join('\n'))
+          }
+        }
+      }
+    });
+});
---
+++ packages/upscalerjs/src/image-utils.ts
@@ -0,0 +1,77 @@
+import { Coordinate, Patch, } from "./types";
+
+const get1DPatch = (total: number, idx: number, patchSize: number, padding: number) => {
+  let preOrigin = idx;
+  const prePadding = idx === 0 ? 0 : padding;
+  const isBeyondBounds = preOrigin + patchSize > total;
+  let postOrigin = isBeyondBounds ? patchSize - (total - preOrigin) : 0;
+  const postPadding = isBeyondBounds ? 0 : padding;
+  const prePaddingAdjusted = isBeyondBounds ? 0 : prePadding;
+  let postSize = patchSize - (isBeyondBounds ? postOrigin : 0);
+  if (isBeyondBounds) {
+    preOrigin = total - patchSize;
+  }
+  preOrigin -= prePaddingAdjusted;
+  postOrigin += prePaddingAdjusted;
+  postSize -= prePaddingAdjusted + postPadding;
+  const increment = patchSize - prePadding - postPadding;
+  return {
+    pre: {
+      origin: preOrigin,
+      size: patchSize,
+    },
+    post: {
+      origin: postOrigin,
+      size: postSize,
+    },
+    increment,
+  };
+}
+
+export const getPatchesFromImage = ([width, height]: Coordinate, patchSize: number, padding: number): Patch[] => {
+  const patches: Patch[] = [];
+  let x = 0;
+  let y = 0;
+  while (y < height) {
+    const {
+      pre: {
+        origin: preOriginY,
+        size: preSizeY,
+      },
+      post: {
+        origin: postOriginY,
+        size: postSizeY,
+      },
+      increment: yIncrement,
+    } = get1DPatch(height, y, Math.min(patchSize, height), padding);
+
+    while (x < width) {
+      const {
+        pre: {
+          origin: preOriginX,
+          size: preSizeX,
+        },
+        post: {
+          origin: postOriginX,
+          size: postSizeX,
+        },
+        increment: xIncrement,
+      } = get1DPatch(width, x, Math.min(patchSize, width), padding);
+
+      patches.push({
+        pre: {
+          origin: [preOriginY, preOriginX,],
+          size: [preSizeY, preSizeX,],
+        },
+        post: {
+          origin: [postOriginY, postOriginX,],
+          size: [postSizeY, postSizeX,],
+        }
+      });
+      x += xIncrement;
+    }
+    x = 0;
+    y += yIncrement;
+  }
+  return patches;
+}
