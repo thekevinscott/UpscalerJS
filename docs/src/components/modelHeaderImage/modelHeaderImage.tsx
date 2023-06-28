96dbab2e3dcef38dfc5859c1a91f210e77bca708
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-06-28 19:24:40 -0400

    Refactor header image component to accept enhanced and unenhanced srces (#1003)

--- docs/src/components/modelHeaderImage/modelHeaderImage.tsx
+++ docs/src/components/modelHeaderImage/modelHeaderImage.tsx
@@ -3,16 +3,15 @@ import styles from './modelHeaderImage.module.scss';
 
 interface IProps {
   packageName: string;
-  model: string;
+  unenhancedSrc: string;
+  enhancedSrc: string;
 }
 
-export default function ModelHeaderImage ({ packageName, model }: IProps) {
-  const originalSrc = `/assets/sample-images/${packageName}/fixture.png`;
-  const enhancedSrc = `/assets/sample-images/${packageName}/samples/${model}/result.png`;
+export default function ModelHeaderImage ({ unenhancedSrc, enhancedSrc, packageName }: IProps) {
   return (
     <div className={styles.modelHeaderImage}>
-      <img src={originalSrc} alt={`Unenhanced file for ${packageName}`} />
-      <img src={enhancedSrc} alt={`Enhanced file for ${packageName}`} />
+      <img src={`/assets/sample-images/${unenhancedSrc}`} alt={`Unenhanced file for ${packageName}`} />
+      <img src={`/assets/sample-images/${enhancedSrc}`} alt={`Enhanced file for ${packageName}`} />
     </div>
   );
 }
