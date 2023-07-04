747bddb59175bd255bb3b715ba0317cdb02a7116
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-07-04 12:18:01 -0400

    Deepsource complaint

--- docs/src/components/homepage/header/demo-video/example-images/example-images.tsx
+++ docs/src/components/homepage/header/demo-video/example-images/example-images.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import React, { useMemo } from 'react';
+import React from 'react';
 import styles from './example-images.module.scss';
 
 export type ValidExampleImageSize = 128 | 256;
