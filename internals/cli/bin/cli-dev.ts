63c1b1cb11860bd32bc1247078608f06c1f7239d
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-09-15 13:04:18 -0400

    Update how CLI gets called

--- internals/cli/bin/cli-dev.ts
+++ internals/cli/bin/cli-dev.ts
@@ -1,7 +1,7 @@
-import { CLI } from '../dist/src/lib/cli/CLI.js';
+#!/usr/bin/env node
+import { CLI } from '../src/lib/cli/CLI.js';
 
 (async () => {
   const cli = new CLI();
   await cli.run();
 })();
-
--- internals/cli/bin/cli.ts
+++
@@ -1,7 +0,0 @@
-#!pnpx ts-node
-import { CLI } from '../src/lib/cli/CLI.js';
-
-(async () => {
-  const cli = new CLI();
-  await cli.run();
-})();
