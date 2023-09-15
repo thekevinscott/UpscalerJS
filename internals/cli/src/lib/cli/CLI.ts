e91debb142e5622f5a61f0cb8401ef2862ffe912
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-09-15 13:08:07 -0400

    Fix references for package json from cli

--- internals/cli/src/lib/cli/CLI.ts
+++ internals/cli/src/lib/cli/CLI.ts
@@ -3,11 +3,11 @@ import path from 'path';
 import { readFileSync } from '@internals/common/fs';
 import { buildCommandsTree } from './build-commands-tree.js';
 import { DEFAULT_LOG_LEVEL, setLogLevel } from '@internals/common/logger';
-import { ROOT_DIR } from '@internals/common/constants';
+import { CLI_DIR } from '@internals/common/constants';
 
 export class CLI {
   constructor() {
-    const packageJSON = path.resolve(ROOT_DIR, './package.json');
+    const packageJSON = path.resolve(CLI_DIR, './package.json');
     const { name, description, version } = JSON.parse(readFileSync(packageJSON, 'utf-8'));
 
     program
@@ -23,7 +23,7 @@ export class CLI {
   }
 
   run = async () => { // skipcq: JS-0105
-    const srcDir = path.resolve(ROOT_DIR, './internals/cli/src/commands');
+    const srcDir = path.resolve(CLI_DIR, './src/commands');
     const root = await buildCommandsTree(srcDir);
     await root.registerProgram(program);
     return program.parseAsync();
