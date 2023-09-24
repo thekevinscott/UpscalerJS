65dc420de6efd862a9e0dbbf7108e772b890bc98
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-09-24 17:57:11 -0400

    TRy updating jest tsconfigs

--- test/jestconfig.browser.browserstack.js
+++ test/jestconfig.browser.browserstack.js
@@ -4,4 +4,26 @@ module.exports = {
   roots: [
     "<rootDir>/integration/browserstack",
   ],
+  "transform": {
+     "^.+\\.(ts|tsx)?$": [
+      "ts-jest", {
+      "tsconfig": {
+        "importHelpers": false,
+        "target": "esnext",
+        "module": "esnext",
+        "moduleResolution": "node",
+        "esModuleInterop": true,
+        "emitDecoratorMetadata": true,
+        "experimentalDecorators": true,
+        "allowSyntheticDefaultImports": true,
+        "sourceMap": true,
+        "noEmit": true,
+        "noEmitHelpers": false,
+        "strictNullChecks": false
+      }
+    }
+    ],
+     "^.+\\.(js|jsx)$": "babel-jest"
+  },
+
 };
