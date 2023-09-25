dae239a947121bb32f9efc8aae119a53286c43c9
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-09-25 08:43:21 -0400

    Run with threads as false

--- packages/upscalerjs/vitest.browser.ts
+++ packages/upscalerjs/vitest.browser.ts
@@ -2,6 +2,7 @@ import { defineConfig, } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    threads: false,
     include: ['**/*.test.ts',],
     exclude: ['**/*.node.test.ts', '**/*.playwright.browser.test.ts',],
     globals: true,
--- packages/upscalerjs/vitest.node.ts
+++ packages/upscalerjs/vitest.node.ts
@@ -2,6 +2,7 @@ import { defineConfig, } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    threads: false,
     include: ['**/*.test.ts',],
     exclude: ['**/*.browser.test.ts',],
     globals: true,
