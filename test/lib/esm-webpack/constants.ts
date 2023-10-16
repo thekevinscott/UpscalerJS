f380637c752587b56d33d38dbe5417a23624fc9f
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-10-16 07:20:18 -0400

    Remove unused files

--- test/lib/esm-webpack/constants.ts
+++
@@ -1,3 +0,0 @@
-// we need to copy the upscaler into the local folder so that it references the correct tfjs installation
-export const LOCAL_UPSCALER_NAME = 'upscaler-for-esm-webpack';
-export const LOCAL_UPSCALER_NAMESPACE = '@upscalerjs-for-esm-webpack';
