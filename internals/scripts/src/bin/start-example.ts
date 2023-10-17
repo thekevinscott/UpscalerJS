03dd6504a2e61368a27b14b4e44a1e931c1ca77f
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-10-17 05:07:13 -0400

    Set up an internals/scripts package, and migrate start-example over

---
+++ internals/scripts/src/bin/start-example.ts
@@ -0,0 +1,48 @@
+/*****
+ * This script spins up a local instance of a particular example, and sets up a watch command to build UpscalerJS.
+ */
+
+import path from 'path';
+import { spawn } from 'child_process';
+import { EXAMPLES_DIR } from '@internals/common/constants';
+import { stat, exists } from '@internals/common/fs';
+import { parseArgs } from "node:util";
+
+/****
+ * Main function
+ */
+
+const startExample = async (example: string) => {
+  const examplePath = path.resolve(EXAMPLES_DIR, example);
+
+  const command = 'npm install --force --no-package-lock && npm run dev'.split(' ');
+
+  spawn(command[0], command.slice(1), {
+    shell: true,
+    cwd: examplePath,
+    stdio: "inherit"
+  });
+};
+
+
+const isValidExample = async (exampleDirectory: string) => {
+  const examplePath = path.resolve(EXAMPLES_DIR, exampleDirectory);
+  return await exists(examplePath) && (await stat(examplePath)).isDirectory();
+};
+
+const main = async () => {
+  const {
+    positionals: [
+      exampleDirectory,
+    ]
+  } = parseArgs({
+    allowPositionals: true,
+  });
+
+  if (!await isValidExample(exampleDirectory)) {
+    throw new Error(`Example directory does not exist: "${exampleDirectory}"`);
+  }
+  await startExample(exampleDirectory);
+};
+
+main();
