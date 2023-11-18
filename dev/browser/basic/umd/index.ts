89b471c79e936c267ceb6304f353b8d0975e2e9a
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-11-18 05:14:18 -0500

    Bump package versions for deploy

--- dev/browser/basic/umd/index.ts
+++ dev/browser/basic/umd/index.ts
@@ -1,3 +1,9 @@
 const upscaler = new window['Upscaler']({
-  model: window['DefaultUpscalerJSModel'],
+  model: {
+    ...window['DefaultUpscalerJSModel'],
+    path: '/node_modules/upscaler/node_modules/@upscalerjs/default-model/models/model.json',
+  },
 });
+
+import flower from '../flower-small.png';
+upscaler.upscale(flower).then(console.log);
