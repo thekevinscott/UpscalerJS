e961eb15ac464a73871f14201cd1d360a880125c
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-12-01 10:19:50 -0500

    Update MAXIM models to use patch sizes

--- models/maxim-deblurring/demo/index.js
+++ models/maxim-deblurring/demo/index.js
@@ -1,12 +1,12 @@
 import Upscaler from "upscaler";
 import model from '@upscalerjs/maxim-deblurring';
-import fixture from "./fixture.png";
 
 const upscaler = new Upscaler({
   model,
 });
 
-upscaler.upscale(fixture).then((upscaledImgSrc) => {
+const fixture = document.getElementById('fixture');
+upscaler.upscale(fixture, { patchSize: 64, padding: 2, progress: console.log }).then((upscaledImgSrc) => {
   const img = document.createElement("img");
   img.src = upscaledImgSrc;
   document.getElementById("target").appendChild(img);
