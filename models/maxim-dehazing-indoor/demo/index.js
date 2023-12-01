996ceae8574afef0d1477ec551db80493310d234
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-12-01 10:20:25 -0500

    Update MAXIM models to use patch sizes (#1278)

--- models/maxim-dehazing-indoor/demo/index.js
+++ models/maxim-dehazing-indoor/demo/index.js
@@ -6,7 +6,7 @@ const upscaler = new Upscaler({
   model,
 });
 
-upscaler.upscale(fixture).then((upscaledImgSrc) => {
+upscaler.upscale(fixture, { patchSize: 64, padding: 2, progress: console.log }).then((upscaledImgSrc) => {
   const img = document.createElement("img");
   img.src = upscaledImgSrc;
   document.getElementById("target").appendChild(img);
